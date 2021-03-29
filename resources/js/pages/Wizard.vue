ed3fcc565d2e49193cf881ea6fabb38eaaa70105
konradmilewski <konrad@kmilewski.eu>
2021-03-29 02:15:46 +0200

    avatar + migrations

--- resources/js/pages/Wizard.vue
+++ resources/js/pages/Wizard.vue
@@ -68,30 +68,22 @@
                         </div>
                     </div>
                     <div class="intro-y col-span-4 sm:col-span-4">
-                        <img
-                            :src="state._image"
-                        />
-                        <button
-                            v-show="!state._file.state || state._file.state === 'success' || state._file.state === 'error'"
-                            @click="select"
-                        >
-                            Upload
-                        </button>
-<!--                        <Dropzone-->
-<!--                            ref-key="dropzoneSingleRef"-->
-<!--                            :options="{-->
-<!--                  url: '/api/avatar/store',-->
-<!--                  thumbnailWidth: 150,-->
-<!--                  maxFilesize: 4,-->
-<!--                  maxFiles: 1,-->
 
-<!--                }"-->
-<!--                            class="dropzone"-->
-<!--                        >-->
-<!--                            <div class="text-lg font-medium">-->
-<!--                               Wgraj swój awatar-->
-<!--                            </div>-->
-<!--                        </Dropzone>-->
+                        <Dropzone
+                            ref-key="dropzoneSingleRef"
+                            :options="{
+                  url: '/api/avatar/store',
+                  thumbnailWidth: 150,
+                  maxFilesize: 4,
+                  maxFiles: 1,
+
+                }"
+                            class="dropzone"
+                        >
+                            <div class="text-lg font-medium">
+                               Kliknij lub upuść swój awatar
+                            </div>
+                        </Dropzone>
                     </div>
 
                     <div
@@ -108,19 +100,17 @@
 </template>
 
 <script>
-    import { defineComponent, ref, onMounted, provide, onBeforeUnmount, reactive, computed } from "vue";
+    import { defineComponent, ref, onMounted, provide } from "vue";
     import DarkModeSwitcher from "../components/dark-mode-switcher/Main.vue";
     import cash from "cash-dom";
     import Dropzone from '../global-components/dropzone/Main'
     import { useToast } from "vue-toastification";
     import {useStore} from "../store";
     import { GoogleMap, Marker } from 'vue3-google-map'
-    import {useUpload} from "@websanova/vue-upload/src/v3";
-
 
     const toast = useToast();
     const store = useStore();
-    const upload = useUpload();
+
 
     export default {
         components: {
@@ -133,92 +123,23 @@
         setup() {
             const toast = useToast();
             const dropzoneSingleRef = ref();
-            console.log(upload);
-            console.log("upload");
-            const state = reactive({
-                file: {
-                    image: null
-                },
-                _file: computed(() => {
-                    return upload.file('demo-single');
-                }),
-                _image: computed(() => {
-                    return state.file.image || '//www.gravatar.com/avatar/?d=robohash&s=320';
-                })
+            provide("bind[dropzoneSingleRef]", el => {
+                dropzoneSingleRef.value = el;
             });
 
-            // provide("bind[dropzoneSingleRef]", el => {
-            //     dropzoneSingleRef.value = el;
-            // });
-            //
-            function select() {
-                upload.select('demo-single');
-            }
-
             onMounted(() => {
-            //         const elDropzoneSingleRef = dropzoneSingleRef.value;
-            //         elDropzoneSingleRef.dropzone.on("success", (resp) => {
-            //
-            //         });
-            //         elDropzoneSingleRef.dropzone.on("error", () => {
-            //             toast.error("Błąd");
-            //         });
-                upload.on('demo-single', {
-                    url: 'api/avatar/store',
-                    accept: 'image/*',
-                    startOnSelect: true,
-                    maxSizePerFile: 1024 * 1024 * 3,
-                    extensions: ['gif', 'png', 'jpg', 'jpeg'],
-                    onSelect: (files, res) => {
-                        console.log('onSelect');
-                        console.log(files);
-                        // Add some additional data to the request.
-                        upload.option('demo-single', 'body', {
-                            some_id: 1
-                        });
-                        // Load a preview first.
-                        upload.file('demo-single').preview((file) => {
-                            state.file.image = file.$raw;
-                        });
-                    },
-                    onProgress(file, res) {
-                        console.log('onProgress');
-                        console.log(file);
-                        console.log(res);
-                    },
-                    onSuccess: (file, res) => {
-                        console.log('onSuccess');
-                        console.log(file);
-                        console.log(res);
-                        // On success we can update whatever we need
-                        // to locally, for instance the user avatar.
-                        state.file = res.data.data;
-                    },
-                    onError: (file, res) => {
-                        console.log('onError');
-                        console.log(file);
-                        console.log(res);
-                    },
-                    onEnd(files, res) {
-                        console.log('onEnd');
-                    }
-                });
+                    const elDropzoneSingleRef = dropzoneSingleRef.value;
+                    elDropzoneSingleRef.dropzone.on("success", (resp) => {
+
+                    });
+                    elDropzoneSingleRef.dropzone.on("error", () => {
+                        toast.error("Błąd");
+                    });
                 cash("body")
                     .removeClass("main")
                     .removeClass("error-page")
                     .addClass("login");
             });
-
-            onBeforeUnmount(() => {
-                upload.off('demo-single');
-            });
-
-
-
-            return {
-                state,
-                select,
-            };
         },
         mounted() {
             console.log(store.state);
