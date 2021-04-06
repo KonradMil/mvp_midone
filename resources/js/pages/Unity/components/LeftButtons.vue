3802408c9ad73bf9cedd365903882cdfe2eb85c3
konradmilewski <konrad@kmilewski.eu>
2021-04-07 00:31:26 +0200

    fix

--- resources/js/pages/Unity/components/LeftButtons.vue
+++ resources/js/pages/Unity/components/LeftButtons.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex fixed h-full z-50">
         <div class="flex-1">
-            <UnityButton tooltip="cos" alttext="alt_text" path="/builder_icons/chair_simple.png" class="w-10 h-10"/>
+            <UnityButton tooltip="cos" alttext="alt_text" path="/s3/builder_icons/chair_simple.png" class="w-10 h-10"/>
         </div>
     </div>
 </template>
