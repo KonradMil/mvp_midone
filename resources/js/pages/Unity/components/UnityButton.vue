65c7a16335c8cdbc61f0dde8be215fe796263c81
konradmilewski <konrad@kmilewski.eu>
2021-04-07 14:50:04 +0200

    fix

--- resources/js/pages/Unity/components/UnityButton.vue
+++ resources/js/pages/Unity/components/UnityButton.vue
@@ -1,10 +1,11 @@
 <template>
     <Tippy
+        v-if="tooltip != ''"
         id="meta-title-tab"
         tag="a"
         :content="tooltip"
         href="javascript:;"
-        class="w-20 py-4 text-center flex justify-center items-center"
+        class="w-20 py-2 text-center flex justify-center items-center"
         aria-selected="false"
         @click=""
     >
@@ -15,6 +16,14 @@
             />
         </div>
     </Tippy>
+    <div v-if="tooltip == ''" class="w-20 py-2 text-center flex justify-center items-center">
+        <div class="w-16 h-16 flex-none image-fit overflow-hidden">
+            <img class="hover:translate-x-1"
+                 :alt="alttext"
+                 :src="path"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
