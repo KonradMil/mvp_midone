02089f0a2e2e48ea28cce15144a5ce4bc592c712
konradmilewski <konrad@kmilewski.eu>
2021-04-23 01:52:15 +0200

    f

--- resources/js/pages/Unity/components/right-panel/LabelDialog.vue
+++ resources/js/pages/Unity/components/right-panel/LabelDialog.vue
@@ -11,7 +11,140 @@
     </div>
     <div class="mt-3">
         <label for="modal-form-2" class="form-label">Kolor</label>
-        <VSwatches id="modal-form-2" v-model="l.textColor" inline></VSwatches>
+<!--        <VSwatches id="modal-form-2" v-model="l.textColor" inline></VSwatches>-->
+        <div class="vue-swatches" tabindex="-1" id="modal-form-2"  modelvalue="#222F3D" data-v-530b672a=""><!-- Trigger --><!--v-if-->
+            <div class="vue-swatches__container vue-swatches--inline" style="background-color: rgb(255, 255, 255);"><!-- The wrapper handles the internal spacing -->
+                <div class="vue-swatches__wrapper"><!-- for nested distribution --><!-- for normal distribution -->
+                    <div class="vue-swatches__swatch" aria-label="#1FBC9C" @click="l.textColor = '#1FBC9C'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(31, 188, 156); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#1CA085"  @click="l.textColor = '#1CA085'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(28, 160, 133); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#2ECC70"  @click="l.textColor = '#2ECC70'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(46, 204, 112); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#27AF60"  @click="l.textColor = '#27AF60'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(39, 175, 96); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#3398DB" @click="l.textColor = '#3398DB'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(51, 152, 219); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#2980B9"  @click="l.textColor = '#2980B9'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(41, 128, 185); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#A463BF" @click="l.textColor = '#A463BF'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(164, 99, 191); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#8E43AD" @click="l.textColor = '#8E43AD'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(142, 67, 173); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#3D556E" @click="l.textColor = '#3D556E'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(61, 85, 110); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#222F3D" @click="l.textColor = '#222F3D'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(34, 47, 61); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#F2C511" @click="l.textColor = '#F2C511'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(242, 197, 17); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#F39C19" @click="l.textColor = '#F39C19'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(243, 156, 25); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#E84B3C" @click="l.textColor = '#E84B3C'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(232, 75, 60); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#C0382B" @click="l.textColor = '#C0382B'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(192, 56, 43); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#DDE6E8" @click="l.textColor = '#DDE6E8'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(221, 230, 232); cursor: pointer; margin-right: 11px; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                    <div class="vue-swatches__swatch" aria-label="#BDC3C8" @click="l.textColor = '#BDC3C8'" role="button" tabindex="0" style="width: 42px; height: 42px; border-radius: 11px; background-color: rgb(189, 195, 200); cursor: pointer; margin-bottom: 11px;"><!--v-if-->
+                        <div class="vue-swatches__check__wrapper vue-swatches--has-children-centered" style="display: none;">
+                            <div class="vue-swatches__check__circle vue-swatches--has-children-centered">
+                                <svg version="1.1" role="presentation" width="12" height="12" viewBox="0 0 1792 1792" class="check">
+                                    <path class="vue-swatches__check__path" d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                                </svg>
+                            </div>
+                        </div><!--v-if--></div>
+                </div><!--v-if--></div>
+        </div>
     </div>
     <div class="mt-3">
         <label for="modal-form-3" class="form-label">Wielkość etykiety</label>
@@ -38,11 +171,11 @@ export default {
         watch(l, (lab, prevLabel) => {
             console.log('CHANGE');
             context.emit("update:label", lab);
-        },{ deep: true })
+        }, {deep: true})
 
         onMounted(() => {
             if (props.label.message != undefined && props.label.message != '') {
-                 l.value.message = props.label.message;
+                l.value.message = props.label.message;
             } else {
                 l.value.message = '';
             }
