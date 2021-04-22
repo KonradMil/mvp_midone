69a144bae9c6a27077427a21db5095143f620045
konradmilewski <konrad@kmilewski.eu>
2021-04-23 01:27:34 +0200

    f

--- resources/js/pages/Unity/components/right-panel/LabelDialog.vue
+++ resources/js/pages/Unity/components/right-panel/LabelDialog.vue
@@ -6,22 +6,22 @@
             type="text"
             class="form-control"
             placeholder=""
-            v-model="label.message"
+            v-model="l.message"
         />
     </div>
     <div class="mt-3">
         <label for="modal-form-2" class="form-label">Kolor</label>
-        <VSwatches id="modal-form-2" v-model="label.textColor" inline></VSwatches>
+        <VSwatches id="modal-form-2" v-model="l.textColor" inline></VSwatches>
     </div>
     <div class="mt-3">
         <label for="modal-form-3" class="form-label">Wielkość etykiety</label>
-        <Slider id="modal-form-3" v-model="label.fontSize" :min="0" :max="15" style="width: 100%;"/>
+        <Slider id="modal-form-3" v-model="l.fontSize" :min="0" :max="15" style="width: 100%;"/>
     </div>
 </template>
 <style src="@vueform/slider/themes/default.css"></style>
 <script>
 import VSwatches from "../../../../components/color-swatches/VSwatches";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch, watchEffect} from "vue";
 import Slider from '@vueform/slider'
 
 export default {
@@ -32,35 +32,36 @@ export default {
     props: {
         label: Object
     },
+    emits: ["update:label"],
     setup(props) {
-        const color = ref('');
-        const title = ref('');
-        const size = ref(14);
+        const l = ref({});
+        watch(l, (lab, prevLabel) => {
+            console.log('CHANGE');
+            context.emit("update:label", lab);
+        })
 
         onMounted(() => {
             if (props.label.message != undefined && props.label.message != '') {
-                // title.value = props.title;
+                 l.value.message = props.label.message;
             } else {
-                props.label.message = '';
+                l.value.message = '';
             }
 
             if (props.label.fontSize != undefined && props.label.fontSize != '') {
-                // size.value = props.size;
+                l.value.fontSize = props.label.fontSize;
             } else {
-                props.label.fontSize = 36;
+                l.value.fontSize = 36;
             }
 
             if (props.label.textColor != undefined && props.label.textColor != '') {
-                // color.value = props.color;
+                l.value.textColor = props.label.textColor;
             } else {
-                props.label.textColor = '';
+                l.value.textColor = '';
             }
         });
 
         return {
-            color,
-            title,
-            size
+            l
         }
     }
 }
