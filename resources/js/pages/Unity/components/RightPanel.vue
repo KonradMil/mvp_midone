bd0fc7548d0926fdf3f98fd6951ad6c691709f28
konradmilewski <konrad@kmilewski.eu>
2021-04-23 02:39:02 +0200

    f

--- resources/js/pages/Unity/components/RightPanel.vue
+++ resources/js/pages/Unity/components/RightPanel.vue
@@ -26,6 +26,7 @@
                 <div class="modal-body">
                     <LabelDialog v-if="content == 'label'" v-model:label="label"/>
                     <CommentDialog v-if="content == 'comment'" v-model:comment="comment"/>
+                    <LayoutDialog v-if="content == 'layout'" v-model:layout="layout"/>
                 </div>
                 <!-- END: Slide Over Body -->
                 <!-- BEGIN: Slide Over Footer -->
@@ -54,10 +55,11 @@ import {getCurrentInstance, onMounted, ref} from "vue";
 import cash from "cash-dom";
 import LabelDialog from "./right-panel/LabelDialog";
 import CommentDialog from "./right-panel/CommentDialog";
+import LayoutDialog from "./right-panel/LayoutDialog";
 
 export default {
     name: "RightPanel",
-    components: {CommentDialog, LabelDialog},
+    components: {LayoutDialog, CommentDialog, LabelDialog},
     setup() {
         //GLOBAL
         const app = getCurrentInstance();
--- resources/js/pages/Unity/components/right-panel/LayoutDialog.vue
+++ resources/js/pages/Unity/components/right-panel/LayoutDialog.vue
@@ -275,7 +275,7 @@
         <label for="modal-form-11" class="form-label">Podświetl po wejściu</label>
         <div class="mt-2">
             <div class="form-check">
-                <input id="checkbox-switch-7" class="form-check-switch" type="checkbox" v-model="l.light_on_enter" />
+                <input id="modal-form-11" class="form-check-switch" type="checkbox" v-model="l.light_on_enter" />
 <!--                <label class="form-check-label" for="checkbox-switch-7">Default switch checkbox input</label>-->
             </div>
         </div>
