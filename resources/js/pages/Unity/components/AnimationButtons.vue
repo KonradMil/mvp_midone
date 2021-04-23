598cde3b841789bca2eac09990c74c3a93f25848
konradmilewski <konrad@kmilewski.eu>
2021-04-23 13:10:57 +0200

    f

---
+++ resources/js/pages/Unity/components/AnimationButtons.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-for="(icon, index) in icons" :key="'animationIcon_' + index" class="bot-i w-30 pl-6">
+        <UnityButton    :tooltip="icon.tooltip" :alttext="icon.alttext" :path="icon.src" :action="index" position="animationbuttonclick" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AnimationButtons",
+    components: {
+      UnityButton
+    },
+    props: {
+      icons: Object
+    },
+    setup() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/pages/Unity/components/AnimationPanel.vue
+++ resources/js/pages/Unity/components/AnimationPanel.vue
@@ -1,12 +1,29 @@
 <template>
+    <div class="flex fixed w-full z-50 pb-2 h-24 bottom-0 " :class="(expanded)? 'h-100': 'h-50'" id="bottom-animation">
+        <div class="left flex  pt-2 flex-row ml-24" style="margin-right: auto;">
 
+        </div>
+    </div>
+<AnimationButtons :icons="icons"></AnimationButtons>
 </template>
 
 <script>
+import AnimationButtons from "./AnimationButtons";
+import {ref} from "vue";
 export default {
     name: "AnimationPanel",
-    setup() {
+    components: {AnimationButtons},
+    props: {
+        icons: Object
+    },
+    setup(props, context) {
+        const lines = ref([]);
+        const expanded = ref(false);
 
+        return {
+            expanded,
+            lines
+        }
     }
 }
 </script>
