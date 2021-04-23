5c3f578ee25dff75ad7ff938a8c181f1341ff8c0
konradmilewski <konrad@kmilewski.eu>
2021-04-23 17:42:15 +0200

    f

--- resources/js/pages/Unity/components/UnityButton.vue
+++ resources/js/pages/Unity/components/UnityButton.vue
@@ -16,7 +16,7 @@
             />
         </div>
     </Tippy>
-    <div v-if="tooltip == ''" class="w-20 py-2 text-center flex justify-center items-center">
+    <div v-if="tooltip == ''" class="w-16 py-2 text-center flex justify-center items-center">
         <div class="w-14 h-14 flex-none image-fit overflow-hidden zoom-in" @click.native="doAction(action)">
             <img class=""
                  :alt="alttext"
