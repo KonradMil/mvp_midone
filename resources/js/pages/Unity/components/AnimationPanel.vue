b8fb009c49f55ae0ba06b09b21ce924b94bb16db
konradmilewski <konrad@kmilewski.eu>
2021-04-25 03:53:16 +0200

    f

--- resources/js/pages/Unity/components/AnimationPanel.vue
+++ resources/js/pages/Unity/components/AnimationPanel.vue
@@ -15,10 +15,18 @@
                 <div class="col-span-11 rounded-md mr-5" style="background-color: rgba(147, 15, 104, 0.25);">
                     <div class="grid grid-cols-12 h-full">
                         <div class="col-span-1">
-<div style="border-radius: 50%; background-color: #FFF;     height: 56px;
-    width: 56px;">
-    <span>C: 10</span>
-</div>
+                            <div style="    border-radius: 50%;
+    background-color: rgb(255, 255, 255);
+    height: 56px;
+    width: 56px;
+    margin-left: 25%;
+    margin-top: 25%;">
+    <span style="font-size: 20px;
+    font-weight: bold;
+    top: 33%;
+    left: 6%;
+    position: relative;">C: 10</span>
+                            </div>
                         </div>
                         <div class="col-span-11 h-full" style="overflow-x: auto; overflow-y: hidden;">
                             <div class="w-full  h-full">
@@ -26,8 +34,8 @@
                                     <div class=" h-full" v-for="(animable, index) in line.animables">
                                         <div class="pos-image__preview image-fit w-44 h-4/5 rounded-md m-5" style="overflow: hidden; ">
                                             <img class="w-full h-full"
-                                                :alt="animable.name.replace('models', 'models_images') + '.png'"
-                                                :src="animable.name.replace('models', 'models_images') + '.png'"
+                                                 :alt="animable.name.replace('models', 'models_images') + '.png'"
+                                                 :src="animable.name.replace('models', 'models_images') + '.png'"
                                             />
                                         </div>
                                     </div>
