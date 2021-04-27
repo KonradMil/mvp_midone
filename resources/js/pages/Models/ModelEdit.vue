ea6cf2a148d87f930b2ca57c5ce42b406c605a1a
Lukaszewski <lmioskowski1999@gmail.com>
2021-04-27 11:12:28 +0200

    commit

--- resources/js/pages/Models/ModelEdit.vue
+++ resources/js/pages/Models/ModelEdit.vue
@@ -28,7 +28,6 @@
                         type="text"
                         class="intro-y form-control py-3 px-4 box pr-10 placeholder-theme-13"
                         :placeholder="$t('global.name')"
-                        v-model="model.name"
                     />
                 </div>
 
