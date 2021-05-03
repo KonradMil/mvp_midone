55042002e16ccc7ab4a9fd17eac8eda700f5a7b7
konradmilewski <konrad@kmilewski.eu>
2021-05-03 18:54:15 +0200

    ss

--- resources/js/pages/Unity/components/AnimationPanel.vue
+++ resources/js/pages/Unity/components/AnimationPanel.vue
@@ -85,10 +85,9 @@ export default {
             console.log(animation.layers[index]);
             console.log('TRYYY');
             console.log(object.layers);
-            let a = toRaw(object.layers);
-            console.log(a);
+            console.log(toRaw(object.layers));
             console.log(unref(object.layers));
-            animation.layers = object.layers;
+            animation.layers = toRaw(object.layers);
             // animation.layers.forEach(function (val, ind) {
             //     console.log("val");
             //     console.log(val);
