2064d6fe0b2c06c18780ac8b26fce72b0e1b3553
konradmilewski <konrad@kmilewski.eu>
2021-05-04 15:16:31 +0200

    ss

--- resources/js/pages/Unity/Main.vue
+++ resources/js/pages/Unity/Main.vue
@@ -60,7 +60,7 @@ export default {
         const radialMenuEdit = ref([]);
         const radialMenuAnimation = ref([]);
         const radialMenuLayout = ref([]);
-        const animationSave = reactive({});
+        const animationSave = reactive({layers: []});
 
 
         window_height.value = window.innerHeight;
@@ -234,7 +234,7 @@ export default {
                     if (response.data.success) {
                         console.log(response.data.payload);
                         initialLoad.value = JSON.parse(response.data.payload.save_json);
-                        animationSave.value = JSON.parse(response.data.payload.save_json).layers;
+                        animationSave.layers = JSON.parse(response.data.payload.save_json).layers;
                         handleUnityActionOutgoing({action: 'loadStructure', data: JSON.parse(response.data.payload.save_json)});
                         // console.log('EMIT LOAD');
                         // emitter.emit('saveLoaded', {save: (response.data.payload)});
