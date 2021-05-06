d902807d7f2047d3cdb65e0685c595de2472b042
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-06 12:52:32 +0200

    commit

--- resources/js/pages/Communication/Main.vue
+++ resources/js/pages/Communication/Main.vue
@@ -181,56 +181,6 @@
                                 </tbody>
                             </table>
                         </div>
-                        <div
-                            v-for="(team, index) in teams.list"
-                            :key="'team_' + index"
-                            class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12"
-                            v-if="activeTab==='teams2'"
-                        >
-                            <div class="box">
-                                <div class="flex flex-col lg:flex-row items-center p-5">
-                                    <div class="w-24 h-24 lg:w-12 lg:h-12 image-fit lg:mr-1">
-                                        <Avatar :username="team.name" color="#FFF" background-color="#930f68"/>
-                                    </div>
-                                    <div
-                                        class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0"
-                                    >
-                                        <a href="" class="font-medium">{{ team.name }}</a>
-                                        <div class="text-gray-600 text-xs mt-0.5">
-                                            Utworzono: {{ $dayjs(team.created_at).format('DD.MM.YYYY HH:mm') }}
-                                        </div>
-                                        <div class="text-gray-600 text-xs mt-0.5">
-                                            Członków: {{ team.users.length }}
-                                        </div>
-                                    </div>
-                                    <div class="flex mt-4 lg:mt-0">
-                                        <button class="btn btn-outline-secondary py-1 px-2" @click="showDetails[team.id] = !showDetails[team.id]">
-                                            {{$t('teams.details')}}
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col lg:flex-row items-center p-5" v-if="showDetails[team.id] === true">
-                                    <div class="intro-y box w-full">
-                                        <div class="p-5">
-                                            <div v-for="(member, index) in team.users" class="relative flex items-center" :key="'member_' + index">
-                                                <div class="w-12 h-12 flex-none image-fit">
-                                                    <Avatar :src="'uploads/' + member.avatar" :username="member.name + ' ' + member.lastname" size="40" color="#FFF" background-color="#930f68"/>
-                                                </div>
-                                                <div class="ml-4 mr-auto">
-                                                    <a href="" class="font-medium">{{ member.name + ' ' + member.lastname }}</a>
-                                                    <div class="text-gray-600 mr-5 sm:mr-5" v-if="member.companies.length != 0">
-                                                        {{member.companies[0].company_name}}
-                                                    </div>
-                                                </div>
-                                                <div class="font-medium text-gray-700 dark:text-gray-600">
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div class="intro-y box p-5"  :class="(activeTab==='report')? '':'hidden'">
                             <AddReport></AddReport>
                         </div>
