64b04e8392ceff115cc4169714f2bd0ae65743d7
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-10 14:05:02 +0200

    commit

--- resources/js/pages/Unity/components/right-panel/DescriptionDialog.vue
+++ resources/js/pages/Unity/components/right-panel/DescriptionDialog.vue
@@ -18,9 +18,9 @@
                             :options="{locale: 'pl', placeholder: 'Wybierz...', limit: 'Nie można wybrać więcej', search: false, hideSelected: false, classNames: 'w-full' }"
                         >
                             <option selected disabled>Wybierz...</option>
-<!--                            <option v-for="(det,index) in challengeSelects.select_detail_weight"-->
-<!--                                    :value="det.value">{{ det.name }}-->
-<!--                            </option>-->
+                            <option v-for="(det,index) in challengeSelects.select_detail_weight"
+                                    :value="det.value">{{ det.name }}
+                            </option>
 
                         </TailSelect>
                     </div>
@@ -222,6 +222,8 @@
 <script>
 import {onMounted, ref, watch} from "vue";
 import { useI18n } from 'vue-i18n'
+import {useToast} from "vue-toastification";
+import SaveChallenge from "../../../../compositions/SaveChallenge";
 
 export default {
     name: "DescriptionDialog",
@@ -231,16 +233,69 @@ export default {
     setup(props, context) {
         const c = ref({description: '', name: '', solution_deadline: '', offer_deadline: ''});
         const { t, locale } = useI18n({ useScope: 'global' })
-
+        const toast = useToast();
+        const category = ref();
+        const showModal = ref(false);
+        const tab = ref('desc');
+        const categories = ref();
+        const tags = ref();
+        const tagsSelected = ref();
+        const solution_deadline = ref("");
+        const offer_deadline = ref("");
+        const challengeSelects = ref();
+        const select_work_shifts = ref();
+        const select_number_of_lines = ref();
+        const select_detail_destination = ref();
+        const select_detail_range = ref();
+        const select_detail_position = ref();
+        const select_detail_pick = ref();
+        const select_detail_size = ref();
+        const select_detail_material = ref();
+        const select_pick_quality = ref();
+        const select_detail_weight = ref();
+        const images = ref([]);
+        const teams = ref([]);
+        const teamsAllowed = ref([]);
+        const description = ref('');
+        const name = ref('');
+        const allowed_publishing = ref(false);
+        const publish = ref(false);
         const types = require("../../../../json/types.json");
         const tagss = require("../../../../json/tagsChallenge.json");
         const sels = require("../../../../json/challenge.json");
 
+        const saveChallengeRepo = async () => {
+            SaveChallenge({
+                name: name.value,
+                description: description.value,
+                type: category.value,
+                solution_deadline: solution_deadline.value,
+                offer_deadline: offer_deadline.value,
+                allowed_publishing: allowed_publishing.value,
+                detail_weight: select_detail_weight.value,
+                pick_quality: select_pick_quality.value,
+                detail_material: select_detail_material.value,
+                detail_size: select_detail_size.value,
+                detail_pick: select_detail_pick.value,
+                detail_position: select_detail_position.value,
+                detail_range: select_detail_range.value,
+                detail_destination: select_detail_destination.value,
+                number_of_lines: select_number_of_lines.value,
+                work_shifts: select_work_shifts.value,
+                teams: teamsAllowed.value,
+                tags: tagsSelected.value,
+                images: images.value
+            });
+        }
         watch(c, (ca, prevLabel) => {
             console.log('CHANGE');
             context.emit("update:comment", ca);
         }, {deep: true})
         onMounted(() => {
+            categories.value = types;
+            tags.value = tagss;
+            challengeSelects.value = sels;
+            saveChallengeRepo('');
             if (props.object.name != undefined && props.object.name != '') {
                 c.value.name = props.object.name;
             } else {
@@ -265,6 +320,29 @@ export default {
         });
 
         return {
+            categories,
+            category,
+            tags,
+            teams,
+            teamsAllowed,
+            description,
+            name,
+            allowed_publishing,
+            publish,
+            solution_deadline,
+            offer_deadline,
+            tab,
+            challengeSelects,
+            select_work_shifts,
+            select_number_of_lines,
+            select_detail_destination,
+            select_detail_range,
+            select_detail_position,
+            select_detail_pick,
+            select_detail_size,
+            select_detail_material,
+            select_pick_quality,
+            select_detail_weight,
             c,
             sels,
             tagss,
