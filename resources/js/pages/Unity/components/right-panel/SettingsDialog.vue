05605cb0a3c968b804421441f59c0625db0e1cf7
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-11 13:28:47 +0200

    commit

--- resources/js/pages/Unity/components/right-panel/SettingsDialog.vue
+++ resources/js/pages/Unity/components/right-panel/SettingsDialog.vue
@@ -176,9 +176,11 @@
 
 <script>
 import {onMounted, ref} from "vue";
+import Multiselect from '@vueform/multiselect'
 
 export default {
     name: "SettingsDialog",
+    components: {Multiselect},
     props: {
         object: Object
     },
