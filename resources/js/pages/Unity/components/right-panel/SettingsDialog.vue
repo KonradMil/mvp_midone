44986fcb590f0da725a3d76098263a3c70148757
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-12 11:58:51 +0200

    commit

--- resources/js/pages/Unity/components/right-panel/SettingsDialog.vue
+++ resources/js/pages/Unity/components/right-panel/SettingsDialog.vue
@@ -14,8 +14,6 @@
                         </label>
                         <TailSelect
                             id="input-wizard-1"
-                            v-model="select_detail_pick "
-                            :placeholder="select_detail_pick"
                             :options="{locale: 'pl', placeholder: sels.select_detail_weight.find(i => i.value === technical.detail_weight).name, limit: 'Nie można wybrać więcej', search: false, hideSelected: false, classNames: 'w-full' }"
                         >
                             <option selected disabled>Wybierz...</option>
