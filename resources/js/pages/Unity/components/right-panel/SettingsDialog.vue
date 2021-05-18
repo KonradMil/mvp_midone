38179f30beda647ad02757bf85fa4fe4df960452
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-18 13:23:48 +0200

    commit

--- resources/js/pages/Unity/components/right-panel/SettingsDialog.vue
+++ resources/js/pages/Unity/components/right-panel/SettingsDialog.vue
@@ -174,9 +174,7 @@ export default {
     name: "SettingsDialog",
     // components: {Multiselect},
     props: {
-        object: Object,
         technical: Object,
-        id: Number
     },
     setup(props) {
         const sels = require("../../../../json/challenge.json");
@@ -190,10 +188,6 @@ export default {
         const challengeSelects = ref();
         onMounted(() => {
             challengeSelects.value = sels;
-            // t.value = props.technical
-            // select_detail_pick.value = sels.select_detail_pick.find(i => i.value === object.detail_pick).name
-            // c.value = props.object
-
         });
 
         return {
