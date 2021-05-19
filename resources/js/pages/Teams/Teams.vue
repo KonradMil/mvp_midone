19e5f358f29c0a788df0041a3e71323b3a867afb
konradmilewski <konrad@kmilewski.eu>
2021-05-19 15:49:45 +0200

    events

--- resources/js/pages/Teams/Teams.vue
+++ resources/js/pages/Teams/Teams.vue
@@ -21,17 +21,17 @@
 
                         </div>
                         <div class="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
-                            <div class="w-56 relative text-gray-700 dark:text-gray-300">
-                                <input
-                                    type="text"
-                                    class="form-control w-56 box pr-10 placeholder-theme-13"
-                                    :placeholder="$t('teams.find')"
-                                    v-model="search"
-                                />
-                                <SearchIcon
-                                    class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0"
-                                />
-                            </div>
+<!--                            <div class="w-56 relative text-gray-700 dark:text-gray-300">-->
+<!--                                <input-->
+<!--                                    type="text"-->
+<!--                                    class="form-control w-56 box pr-10 placeholder-theme-13"-->
+<!--                                    :placeholder="$t('teams.find')"-->
+<!--                                    v-model="search"-->
+<!--                                />-->
+<!--                                <SearchIcon-->
+<!--                                    class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0"-->
+<!--                                />-->
+<!--                            </div>-->
                         </div>
                     </div>
                     <!-- BEGIN: Users Layout -->
