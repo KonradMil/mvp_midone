7ea6ad413ebe45d7d18c557fd9139ddf07072d71
konradmilewski <konrad@kmilewski.eu>
2021-05-19 02:20:02 +0200

    events

--- resources/js/pages/Unity/components/AnimationPanel.vue
+++ resources/js/pages/Unity/components/AnimationPanel.vue
@@ -13,7 +13,7 @@
                     <UnityButton tooltip="Minimalizuj" alttext="Minimalizuj" path="/s3/builder_icons/minimalize_simple.png" action="minimalize" position="animationbuttonclick"/>
                     <UnityButton tooltip="Odtwórz" alttext="Odtwórz" path="/s3/builder_icons/play_simple.png" action="play" position="animationbuttonclick"/>
                 </div>
-                <div class="col-span-11 rounded-md mr-5  h-full" style=" overflow-y: scroll;">
+                <div class="col-span-11 rounded-md mr-5" style=" overflow-y: scroll;">
                     <div class="grid grid-cols-12 my-3" :class="(activeLineIndex == index)? 'active':''" style="max-height: 200px;" v-for="(line, index) in animation.layers">
                         <div class="col-span-1">
                             <div style="margin-left: 25%; margin-top: calc(25% - 10px);">
