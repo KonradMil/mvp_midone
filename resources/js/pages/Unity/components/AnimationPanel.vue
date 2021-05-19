0e2d6c5684b1d89415f359039439041295dc40e9
konradmilewski <konrad@kmilewski.eu>
2021-05-19 02:10:28 +0200

    events

--- resources/js/pages/Unity/components/AnimationPanel.vue
+++ resources/js/pages/Unity/components/AnimationPanel.vue
@@ -33,8 +33,8 @@
                         </div>
                         <div class="col-span-10 h-full" style="overflow-x: auto; overflow-y: hidden;">
                             <div class="w-full  h-full">
-                                <div class="row flex h-full" :class="(activeLineIndex == index)? 'active':''">
-                                    <div class=" h-full" v-for="(animable, index) in line.animables">
+                                <div class="row flex h-full" >
+                                    <div class=" h-full" v-for="(animable, index) in line.animables" :class="(activeLineIndex == index)? 'active':''">
                                         <div class="pos-image__preview image-fit w-44 h-46 rounded-md m-5" style="overflow: hidden;" @click="activeAnimableIndex = index; showAnimableDialog();">
                                             <img class="w-full h-full"
                                                  :alt="animable.name.replace('models', 'models_images') + '.png'"
