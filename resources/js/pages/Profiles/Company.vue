561669873909569d7b68c1597356871de84c9387
konradmilewski <konrad@kmilewski.eu>
2021-05-20 13:50:01 +0200

    events

--- resources/js/pages/Profiles/Company.vue
+++ resources/js/pages/Profiles/Company.vue
@@ -1,125 +1,143 @@
 <template>
-    <div class="intro-y box lg:mt-5">
+    <div class="intro-y box lg:mt-5" v-if="activeTab==='personalia'">
         <div class="flex items-center p-5 border-b border-gray-200 dark:border-dark-5">
             <h2 class="font-medium text-base mr-auto">
-                {{ $t('profiles.agreements') }}
+                {{$t('profiles.personality')}}
             </h2>
         </div>
-        <!-- END: Display Information -->
-        <div class="pl-4 pb-5">
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-1" class="form-label">{{ $t('profiles.companyName') }}</label>
-                <input
-                    id="input-wizard-1"
-                    type="text"
-                    class="form-control"
-                    v-model="company.company_name"/>
+        <form @submit.prevent="handleSubmit">
+            <div class="p-5">
+                <div class="flex flex-col-reverse xl:flex-row flex-col">
+                    <div class="flex-1 mt-6 xl:mt-0">
+                        <div class="grid grid-cols-12 gap-x-5">
+                            <div class="col-span-12 xxl:col-span-6">
+                                <div class="mt-3">
+                                    <label for="input-wizard-1" class="form-label">{{ $t('profiles.companyName') }}</label>
+                                    <input
+                                        id="input-wizard-1"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.company_name"/>
+                                </div>
+                                <div class="mt-3">
+                                    <label for="input-wizard-2" class="form-label">NIP</label>
+                                    <input
+                                        id="input-wizard-2"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.nip"
+                                    />
+                                    <button class="btn btn-primary w-1/4 mt-2" @click="searchNip">{{ $t('profiles.searchFor') }} NIP</button>
+                                </div>
+                            </div>
+                            <div class="col-span-12 xxl:col-span-6">
+                                <div class="mt-3">
+                                    <label for="input-wizard-3" class="form-label">REGON</label>
+                                    <input
+                                        id="input-wizard-3"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.regon"
+                                    />
+                                    <button class="btn btn-primary w-1/3 mt-2" @click="searchRegon">{{ $t('profiles.searchFor') }} REGON</button>
+                                </div>
+                                <div class="mt-3">
+                                    <label for="input-wizard-4" class="form-label">KRS</label>
+                                    <input
+                                        id="input-wizard-4"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.krs"
+                                    />
+                                    <button class="btn btn-primary w-1/4 mt-2" @click="searchKRS">{{ $t('profiles.searchFor') }} KRS</button>
+                                </div>
+                            </div>
+                            <div class="col-span-12 xxl:col-span-6">
+                                <div class="mt-3">
+                                    <label for="input-wizard-5" class="form-label">{{ $t('profiles.city') }}</label>
+                                    <input
+                                        id="input-wizard-5"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.city"
+                                    />
+                                </div>
+                                <div class="mt-3">
+                                    <label for="input-wizard-6" class="form-label">{{ $t('profiles.street') }}</label>
+                                    <input
+                                        id="input-wizard-6"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.street"
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-span-12 xxl:col-span-6">
+                                <div class="mt-3">
+                                    <label for="input-wizard-5" class="form-label">{{ $t('profiles.houseNumber') }}</label>
+                                    <input
+                                        id="input-wizard-7"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.house_nr"
+                                    />
+                                </div>
+                                <div class="mt-3">
+                                    <label for="input-wizard-6" class="form-label">{{ $t('profiles.flatNumber') }}</label>
+                                    <input
+                                        id="input-wizard-8"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.loc_nr"
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-span-12 xxl:col-span-6">
+                                <div class="mt-3">
+                                    <label for="input-wizard-5" class="form-label">{{ $t('profiles.postCode') }}</label>
+                                    <input
+                                        id="input-wizard-9"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.postcode"
+                                    />
+                                </div>
+                                <div class="mt-3">
+                                    <label for="input-wizard-6" class="form-label">{{ $t('profiles.flatNumber') }}</label>
+                                    <input
+                                        id="input-wizard-8"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.loc_nr"
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-span-12 xxl:col-span-6">
+                                <div class="mt-3">
+                                    <label for="input-wizard-5" class="form-label">{{ $t('profiles.province') }}</label>
+                                    <input
+                                        id="input-wizard-11"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.voivodeship"
+                                    />
+                                </div>
+                                <div class="mt-3">
+                                    <label for="input-wizard-6" class="form-label">{{ $t('profiles.country') }}</label>
+                                    <input
+                                        id="input-wizard-12"
+                                        type="text"
+                                        class="form-control"
+                                        v-model="company.country"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary w-20 mt-3" @click="saveCompany">{{ $t('global.save') }}</button>
+                    </div>
+                </div>
             </div>
-            <label for="input-wizard-1" class="form-label">{{ $t('profiles.companyName') }}</label>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-2" class="form-label">NIP</label>
-                <input
-                    id="input-wizard-2"
-                    type="text"
-                    class="form-control"
-                    v-model="company.nip"
-                />
-                <button class="btn btn-primary w-1/4 mt-2" @click="searchNip">{{ $t('profiles.searchFor') }} NIP</button>
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-3" class="form-label">REGON</label>
-                <input
-                    id="input-wizard-3"
-                    type="text"
-                    class="form-control"
-                    v-model="company.regon"
-                />
-                <button class="btn btn-primary w-1/3 mt-2" @click="searchRegon">{{ $t('profiles.searchFor') }} REGON</button>
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-4" class="form-label">KRS</label>
-                <input
-                    id="input-wizard-4"
-                    type="text"
-                    class="form-control"
-                    v-model="company.krs"
-                />
-                <button class="btn btn-primary w-1/4 mt-2" @click="searchKRS">{{ $t('profiles.searchFor') }} KRS</button>
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-5" class="form-label">{{ $t('profiles.city') }}</label>
-                <input
-                    id="input-wizard-5"
-                    type="text"
-                    class="form-control"
-                    v-model="company.city"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-6" class="form-label">{{ $t('profiles.street') }}</label>
-                <input
-                    id="input-wizard-6"
-                    type="text"
-                    class="form-control"
-                    v-model="company.street"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-5" class="form-label">{{ $t('profiles.houseNumber') }}</label>
-                <input
-                    id="input-wizard-7"
-                    type="text"
-                    class="form-control"
-                    v-model="company.house_nr"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-6" class="form-label">{{ $t('profiles.flatNumber') }}</label>
-                <input
-                    id="input-wizard-8"
-                    type="text"
-                    class="form-control"
-                    v-model="company.loc_nr"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-5" class="form-label">{{ $t('profiles.postCode') }}</label>
-                <input
-                    id="input-wizard-9"
-                    type="text"
-                    class="form-control"
-                    v-model="company.postcode"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-6" class="form-label">{{ $t('profiles.post') }}</label>
-                <input
-                    id="input-wizard-10"
-                    type="text"
-                    class="form-control"
-                    v-model="company.loc_nr"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-5" class="form-label">{{ $t('profiles.province') }}</label>
-                <input
-                    id="input-wizard-11"
-                    type="text"
-                    class="form-control"
-                    v-model="company.voivodeship"
-                />
-            </div>
-            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm ">
-                <label for="input-wizard-6" class="form-label">{{ $t('profiles.country') }}</label>
-                <input
-                    id="input-wizard-12"
-                    type="text"
-                    class="form-control"
-                    v-model="company.country"
-                />
-            </div>
-        </div>
-        <button class="btn btn-primary w-20 mt-3" @click="saveCompany">{{ $t('global.save') }}</button>
+        </form>
     </div>
 </template>
 
