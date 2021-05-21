dcb1e12fc0e79d6a3702b81b1a73d1f2931eef48
konradmilewski <konrad@kmilewski.eu>
2021-05-21 12:17:35 +0200

    events

--- app/Http/Controllers/Crud/ChallengeController.php
+++ app/Http/Controllers/Crud/ChallengeController.php
@@ -453,7 +453,7 @@ class ChallengeController extends Controller
     {
         if (isset($request->id)) {
             $challenge = Challenge::with(
-                'solutions', 'author', 'technicalDetails', 'financial_before', 'teams', 'files', 'team.users'
+                'solutions', 'author', 'technicalDetails', 'financial_before', 'teams', 'files', 'teams.users'
             )->find($request->id);
 
         } else {
