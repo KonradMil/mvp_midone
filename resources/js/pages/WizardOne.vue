c2fd8a2f4eb31e6f7812bcca0bb5cb52a7186404
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-27 12:28:16 +0200

    commit

--- resources/js/pages/WizardOne.vue
+++ resources/js/pages/WizardOne.vue
@@ -74,7 +74,7 @@
                     </div>
                     <div class="intro-y col-span-12 flex items-center justify-center sm:justify-end mt-5">
                         <button class="btn btn-secondary w-24" @click="$router.push('kreator')">Wstecz</button>
-                        <button class="btn btn-primary w-24 ml-2" @click="save">Dalej</button>
+                        <button class="btn btn-primary w-24 ml-2" @click="save">Zapisz</button>
                     </div>
                 </div>
             </div>
