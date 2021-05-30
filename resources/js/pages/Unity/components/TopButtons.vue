a8ead356eeeb123d1a6af1b17a2b7dd7ca06207a
konradmilewski <konrad@kmilewski.eu>
2021-05-30 20:46:25 +0200

    ff

--- resources/js/pages/Unity/components/TopButtons.vue
+++ resources/js/pages/Unity/components/TopButtons.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex fixed w-full z-50 pt-2 h-24 top-0 " id="top" style="pointer-events: none;">
             <div class="left flex  pt-2 flex-row ml-24" style="margin-right: auto;">
-                <div  v-for="(icon, index) in icons.left" :key="'topIcon_' + index" class="top-i w-30 pl-6unity-button">
+                <div  v-for="(icon, index) in icons.left" :key="'topIcon_' + index" class="top-i w-30 pl-6 unity-button">
                     <UnityButton  v-if="icon.type === 'button'" :tooltip="icon.tooltip" :alttext="icon.alttext" :path="icon.src" :action="index" position="topbuttonclick"/>
                     <UnityDropdown v-if="icon.type === 'dropdown'" :alttext="icon.alttext" :path="icon.src" :action="index" position="topbuttonclick"></UnityDropdown>
                 </div>
