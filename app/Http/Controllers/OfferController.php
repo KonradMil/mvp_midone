9a703fdcf15cc2abb667a2ff2f5582453331df41
Lukaszewski <lmioskowski1999@gmail.com>
2021-05-31 15:12:17 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -40,7 +40,26 @@ class OfferController extends Controller
             $check = new Offer();
         }
 
-        $check->fill($request->input());
+//        $check->fill($request->input());
+        $check->challenge_id = $request->challenge_id;
+        $check->solution_id = $request->solution_id;
+        $check->installer_id = Auth::user()->id;
+        $check->price_of_delivery = $request->price_of_delivery;
+        $check->weeks_to_start = $request->weeks_to_start;
+        $check->time_to_start = $request->time_to_start;
+        $check->time_to_fix = $request->time_to_fix;
+        $check->advance_upon_start = $request->advance_upon_start;
+        $check->advance_upon_delivery = $request->advance_upon_delivery;
+        $check->advance_upon_agreement = $request->advance_upon_agreement;
+        $check->years_of_guarantee = $request->years_of_guarantee;
+        $check->service_support_scope = $request->service_support_scope;
+        $check->maintenance_frequency = $request->maintenance_frequency;
+        $check->price_of_maintenance = $request->price_of_maintenance;
+        $check->reaction_time = $request->reaction_time;
+        $check->intervention_price = $request->intervention_price;
+        $check->work_hour_price = $request->work_hour_price;
+        $check->period_of_support = $request->period_of_support;
+
 
 //        $solution = Solution::find($request->input('solution_id'));
 //        $check->challenge_id = $solution->challenge_id;
@@ -49,7 +68,7 @@ class OfferController extends Controller
 
         return response()->json([
             'success' => true,
-            'message' => 'Pobrano ofertę poprawnie.',
+            'message' => 'Zaktualizowano oferte poprawnie.',
             'payload' => $check
         ]);
     }
