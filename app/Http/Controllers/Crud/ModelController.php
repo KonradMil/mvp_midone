2be12fc499497d2fc72a28b7c50bb8c17977fbc2
konradmilewski <konrad@kmilewski.eu>
2021-06-01 11:02:19 +0200

    ff

--- app/Http/Controllers/Crud/ModelController.php
+++ app/Http/Controllers/Crud/ModelController.php
@@ -32,7 +32,7 @@ class ModelController extends Controller
                 $models = UnityModel::where('name', 'LIKE', '%'. $request->search . '%')->take(10)->get();
             }
         } else {
-            $models = UnityModel::get();
+            $models = UnityModel::take(10)->get();
         }
 
         return response()->json([
