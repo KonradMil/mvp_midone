440f45e8c2c77bb64f386c255bbf2549296b1087
konradmilewski <konrad@kmilewski.eu>
2021-06-01 21:40:45 +0200

    ff

--- resources/js/pages/Challenges/components/Offers.vue
+++ resources/js/pages/Challenges/components/Offers.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="col-span-9 lg:col-span-9 xxl:col-span-9">
         <div class="grid grid-cols-12 gap-6">
+            <div class="flex items-center px-5 py-3 border-b border-gray-200 dark:border-dark-5">
+                <h2 class="font-medium text-base mr-auto"> Moje oferty</h2>
+            </div>
             <!-- BEGIN: Announcement -->
             <div class="intro-y box col-span-6 xxl:col-span-6" v-for="(offer, index) in offers.list" :key="index">
-                <div class="flex items-center px-5 py-3 border-b border-gray-200 dark:border-dark-5">
-                    <h2 class="font-medium text-base mr-auto"> Moje oferty</h2>
-                </div>
+
                 <div class="px-5 py-5">
                     <div id="latest-tasks-new" class="tab-pane active" role="tabpanel" aria-labelledby="latest-tasks-new-tab">
                         <div class="flex items-center">
