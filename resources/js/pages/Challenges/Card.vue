a9253388998af755178c4decd0a7d89d1c646ece
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-02 10:41:59 +0200

    cc

--- resources/js/pages/Challenges/Card.vue
+++ resources/js/pages/Challenges/Card.vue
@@ -181,7 +181,8 @@ export default defineComponent({
         emitter.on('*', (type, e) => {
             console.log(type, e);
             console.log('HERE212');
-            console.log(e.solution + '-> e.solution')
+            console.log('e solution');
+            console.log(e.solution + '-> e.solution');
              if(type == 'activeTab') {
                     activeTab.value = e.name;
                     solution.value = e.solution;
