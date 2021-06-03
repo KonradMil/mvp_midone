ffef9183f157ae228b9742304efdc86bf0072926
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-03 21:36:03 +0200

    cc

--- resources/js/components/social/Comment.vue
+++ resources/js/components/social/Comment.vue
@@ -11,10 +11,8 @@
         <div class="flex-1 relative text-dark-700" v-if="comment.commentator.id == user.id">
             <div class="form-control form-control-rounded border-transparent bg-gray-400 pr-10 placeholder-theme-13">
                 {{ comment.comment }}
-                <TrashIcon></TrashIcon>
             </div>
         </div>
-          <a class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> </a>
         <div class="w-8 h-8 flex-none image-fit ml-3" v-if="comment.commentator.id == user.id">
             <Avatar :src="'/s3/avatars/' + user.avatar" :username="user.name + ' ' + user.lastname" :size="35" color="#FFF" background-color="#930f68"/>
             </div>
