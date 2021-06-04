38b7e6999f71a78ecdcf5b72f6bb0afb6f5edd5d
konradmilewski <konrad@kmilewski.eu>
2021-06-04 19:32:44 +0200

    ff

--- app/Http/Controllers/WorkshopController.php
+++ app/Http/Controllers/WorkshopController.php
@@ -44,6 +44,7 @@ class WorkshopController extends Controller
             $path = $this->processSS($j['screenshot']);
             $object->screenshot_path = $path['relative'];
             unset($j['screenshot']);
+            dump($j);
             $object->save = json_encode($request->input('object')['data']);
             $object->name = $j['name'];
             $object->description = $j['description'];
