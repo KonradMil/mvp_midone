ab524699b8705a54f70a9c4053f264935c08fdf8
konradmilewski <konrad@kmilewski.eu>
2021-06-04 19:45:55 +0200

    ff

--- resources/js/pages/Unity/Workshop/panels/OwnObjects.vue
+++ resources/js/pages/Unity/Workshop/panels/OwnObjects.vue
@@ -4,10 +4,8 @@
             Nie masz jeszcze żadnych zapisanych obiektów.
         </div>
         <div class="grid grid-cols-12 gap-6 mt-5">
-            <div v-for="(object, index) in objects" v-if="objects.length != 0">
-                <div class="col-span-12 lg:col-span-4 xxl:col-span-4 flex lg:block flex-col-reverse">
+            <div v-for="(object, index) in objects" :key="'workshop_object' + index" v-if="objects.length != 0" class="col-span-12 lg:col-span-4 xxl:col-span-4 flex lg:block flex-col-reverse">
                 <SingleWorkshopObject :object="object" :key="'obiekt_' + index"/>
-                </div>
             </div>
         </div>
     </div>
