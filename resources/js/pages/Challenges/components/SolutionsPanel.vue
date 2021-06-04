6f51d7e2a2ce82959b8fa893aed322dfcee7aa77
konradmilewski <konrad@kmilewski.eu>
2021-06-04 21:53:48 +0200

    ff

--- resources/js/pages/Challenges/components/SolutionsPanel.vue
+++ resources/js/pages/Challenges/components/SolutionsPanel.vue
@@ -21,15 +21,12 @@
                             <SingleSolutionPost :user="user" :solution="challenge.selected" :canAccept="false" :canEdit="false"></SingleSolutionPost>
                         </div>
                         <div v-for="(solution, index) in challenge.solutions" :key="index" v-if="challenge.stage < 2" >
-                            <div v-if="!solution.rejected">
-                                <div v-if="user.type == 'integrator'">
-                                    <SingleSolutionPost class="intro-y col-span-6 md:col-span-4 xl:col-span-6 box" :class="(solution.selected)? 'solution-selected': ''" v-if="(solution.author_id === user.id)" :user="user" :solution="solution" :canAccept="(user.id === challenge.author_id) && challenge.status == 1" :canEdit="user.id === solution.author_id"></SingleSolutionPost>
+                                <div v-if="user.type == 'integrator'" class="intro-y col-span-6 md:col-span-4 xl:col-span-6 box" :class="(solution.selected)? 'solution-selected': ''">
+                                    <SingleSolutionPost  v-if="(solution.author_id === user.id)" :user="user" :solution="solution" :canAccept="(user.id === challenge.author_id) && challenge.status == 1" :canEdit="user.id === solution.author_id"></SingleSolutionPost>
                                 </div>
-                                <div v-if="user.type == 'investor'">
-                                    <SingleSolutionPost class="intro-y col-span-6 md:col-span-4 xl:col-span-6 box" :class="(solution.selected)? 'solution-selected': ''"  v-if="solution.status === 1" :user="user" :solution="solution" :canAccept="(user.id === challenge.author_id) && challenge.status == 1" :canEdit="user.id === solution.author_id"></SingleSolutionPost>
+                                <div v-if="user.type == 'investor'" class="intro-y col-span-6 md:col-span-4 xl:col-span-6 box" :class="(solution.selected)? 'solution-selected': ''">
+                                    <SingleSolutionPost  v-if="solution.status === 1" :user="user" :solution="solution" :canAccept="(user.id === challenge.author_id) && challenge.status == 1" :canEdit="user.id === solution.author_id"></SingleSolutionPost>
                                 </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
