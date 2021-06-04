02556e1abe90ad0cf74b4808ad3a3ca81f643032
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-04 21:02:32 +0200

    cc

--- resources/js/components/social/CommentSection.vue
+++ resources/js/components/social/CommentSection.vue
@@ -94,7 +94,10 @@ export default {
 
         emitter.on('deletecomment', e => {
             console.log(comments.value + '-> comments.value');
-            comments.value[e.index].splice(e.index, 1);
+            if(Array.isArray(comments.value))
+            {
+                comments.value.splice(e.index, 1);
+            }
         });
         emitter.on('disliked', e =>  dislike(e.id) )
 
