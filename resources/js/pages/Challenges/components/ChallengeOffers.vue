9bfb0effe6bf4531c780ca304a270d7d2e5078b1
konradmilewski <konrad@kmilewski.eu>
2021-06-05 03:25:33 +0200

    ff

--- resources/js/pages/Challenges/components/ChallengeOffers.vue
+++ resources/js/pages/Challenges/components/ChallengeOffers.vue
@@ -32,15 +32,15 @@
 <!--            </div>-->
 <!--        </div>-->
 <!--    </div>-->
-    <div class="col-span-12 lg:col-span-8 xxl:col-span-9" v-for="(offer, index) in offers.list" :key="index">
+    <div class="col-span-12 lg:col-span-8 xxl:col-span-9" >
         <div class="grid grid-cols-12 gap-6">
-            <!-- BEGIN: Announcement -->
-            <div class="intro-y box col-span-12 xxl:col-span-6">
-                <div
-                    class="flex items-center px-5 py-3 border-b border-gray-200 dark:border-dark-5"
-                >
-                    <h2 class="font-medium text-base mr-auto"> Moje oferty</h2>
-                </div>
+            <!-- BEGIN: Announcement --> <div
+            class="flex items-center px-5 py-3 border-b border-gray-200 dark:border-dark-5"
+        >
+            <h2 class="font-medium text-base mr-auto"> Moje oferty</h2>
+        </div>
+            <div class="intro-y box col-span-12 xxl:col-span-6" v-for="(offer, index) in offers.list" :key="index">
+
                 <div class="px-5 py-5"  >
                     <div
                         id="latest-tasks-new"
@@ -143,16 +143,6 @@
             </div>
             <!-- END: Announcement -->
             <!-- BEGIN: Daily Sales -->
-            <div class="intro-y box col-span-12 xxl:col-span-6">
-                <div
-                    class="flex items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5"
-                >
-                    <!--                    <h2 class="font-medium text-base mr-auto">Zdjęcia</h2>-->
-                </div>
-                <div class="p-10">
-
-                </div>
-            </div>
             <!-- END: Daily Sales -->
         </div>
     </div>
