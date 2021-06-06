c3bedb01a36aa8415f0c2998390bed9341cf23a5
konradmilewski <konrad@kmilewski.eu>
2021-06-06 20:22:57 +0200

    ff

--- resources/js/pages/Challenges/components/TeamsPanel.vue
+++ resources/js/pages/Challenges/components/TeamsPanel.vue
@@ -1,25 +1,26 @@
 <template>
     <div class="grid grid-cols-12 gap-6 mt-5 col-span-8">
-            <div class="col-span-12">
-                <h2 class="intro-y text-lg font-medium mt-5">{{$t('teams.teams')}}</h2>
-                <div class="grid grid-cols-12 gap-6 mt-5">
-                    <div
-                        class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2"
-                    >
-                        <div class="w-56 relative text-gray-700 dark:text-gray-300 mr-4">
-                            <input
-                                type="text"
-                                class="form-control w-56 box pr-10 placeholder-theme-13"
-                                :placeholder="$t('teams.name')"
-                                v-model="new_team_name"
-                            />
-                        </div>
-                        <button class="btn btn-primary shadow-md mr-2" :disabled="isDisabled" @click="addObjectTeam">{{$t('teams.addTeam')}}</button>
+        <div class="col-span-12">
+            <h2 class="intro-y text-lg font-medium mt-5">{{ $t('teams.teams') }}</h2>
+            <div class="grid grid-cols-12 gap-6 mt-5">
+                <div
+                    class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2"
+                >
+                    <div class="w-56 relative text-gray-700 dark:text-gray-300 mr-4">
+                        <input
+                            type="text"
+                            class="form-control w-56 box pr-10 placeholder-theme-13"
+                            :placeholder="$t('teams.name')"
+                            v-model="new_team_name"
+                        />
                     </div>
-                    <div class="grid grid-cols-12 gap-6 mt-2">
-                        <div class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12">
-                            <h5>Dostępne zespoły</h5>
-                        </div>
+                    <button class="btn btn-primary shadow-md mr-2" :disabled="isDisabled" @click="addObjectTeam">{{ $t('teams.addTeam') }}</button>
+                </div>
+                <div class="grid grid-cols-12 gap-6 mt-2">
+                    <div class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12">
+                        <h5>Dostępne zespoły</h5>
+                    </div>
+                    <div class="col-span-12">
                         <div class="grid grid-cols-12 gap-6 mt-2">
                             <div v-for="(team, index) in teams" :key="'team_' + index" class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12">
                                 <div class="box">
@@ -30,16 +31,16 @@
                                         <div class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0">
                                             <a href="" class="font-medium">{{ team.name }}</a>
                                             <div class="text-gray-600 text-xs mt-0.5">
-                                                {{$t('teams.created')}}: {{ $dayjs(team.created_at).format('DD.MM.YYYY HH:mm') }}
+                                                {{ $t('teams.created') }}: {{ $dayjs(team.created_at).format('DD.MM.YYYY HH:mm') }}
                                             </div>
                                             <div class="text-gray-600 text-xs mt-0.5" v-if="team.users != undefined">
-                                                {{ $t('teams.members')}}: {{ team.users.length }}
+                                                {{ $t('teams.members') }}: {{ team.users.length }}
                                             </div>
                                         </div>
                                         <div class="flex mt-4 lg:mt-0">
-                                            <button class="btn btn-primary py-1 px-2 mr-2" @click="showAddToTeamModal(team.id)" v-if="team.owner_id === user.id">{{$t('teams.add')}}</button>
+                                            <button class="btn btn-primary py-1 px-2 mr-2" @click="showAddToTeamModal(team.id)" v-if="team.owner_id === user.id">{{ $t('teams.add') }}</button>
                                             <button class="btn btn-outline-secondary py-1 px-2" @click="showDetails[team.id] = !showDetails[team.id]">
-                                                {{$t('teams.details')}}
+                                                {{ $t('teams.details') }}
                                             </button>
                                         </div>
                                     </div>
@@ -53,11 +54,13 @@
                                                     <div class="ml-4 mr-auto">
                                                         <a href="" class="font-medium">{{ member.name + ' ' + member.lastname }}</a>
                                                         <div class="text-gray-600 mr-5 sm:mr-5" v-if="member.companies.length != 0">
-                                                            {{member.companies[0].company_name}}
+                                                            {{ member.companies[0].company_name }}
                                                         </div>
                                                     </div>
                                                     <div class="flex justify-center items-center" v-if="member.id != user.id">
-                                                        <a :disabled="isDisabled" @click.prevent="del(member.id,team.id, index)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon> Delete </a>
+                                                        <a :disabled="isDisabled" @click.prevent="del(member.id,team.id, index)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal">
+                                                            <TrashIcon></TrashIcon>
+                                                            Delete </a>
                                                     </div>
                                                     <div class="font-medium text-gray-700 dark:text-gray-600">
                                                     </div>
@@ -68,13 +71,16 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12">
-                            <h5 v-if="who == 'challenge'">
-                                Zespoły dodane do wyzwania
-                            </h5>
-                            <h5 v-if="who == 'solution'">
-                                Zespoły dodane do rozwiązania
-                            </h5>
+                    </div>
+
+                    <div class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12">
+                        <h5 v-if="who == 'challenge'">
+                            Zespoły dodane do wyzwania
+                        </h5>
+                        <h5 v-if="who == 'solution'">
+                            Zespoły dodane do rozwiązania
+                        </h5>
+                        <div class="col-span-12">
                             <div class="grid grid-cols-12 gap-6 mt-2">
                                 <div v-for="(team, index) in teamsObject" :key="'team_' + index" class="intro-y col-span-6 xl:col-span-6 md:col-span-6 sm:col-span-12">
                                     <div class="box">
@@ -85,16 +91,16 @@
                                             <div class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0">
                                                 <a href="" class="font-medium">{{ team.name }}</a>
                                                 <div class="text-gray-600 text-xs mt-0.5">
-                                                    {{$t('teams.created')}}: {{ $dayjs(team.created_at).format('DD.MM.YYYY HH:mm') }}
+                                                    {{ $t('teams.created') }}: {{ $dayjs(team.created_at).format('DD.MM.YYYY HH:mm') }}
                                                 </div>
                                                 <div class="text-gray-600 text-xs mt-0.5" v-if="team.users != undefined">
-                                                    {{ $t('teams.members')}}: {{ team.users.length }}
+                                                    {{ $t('teams.members') }}: {{ team.users.length }}
                                                 </div>
                                             </div>
                                             <div class="flex mt-4 lg:mt-0">
-                                                <button class="btn btn-primary py-1 px-2 mr-2" @click="showAddToTeamModal(team.id)" v-if="team.owner_id === user.id">{{$t('teams.add')}}</button>
+                                                <button class="btn btn-primary py-1 px-2 mr-2" @click="showAddToTeamModal(team.id)" v-if="team.owner_id === user.id">{{ $t('teams.add') }}</button>
                                                 <button class="btn btn-outline-secondary py-1 px-2" @click="showDetails[team.id] = !showDetails[team.id]">
-                                                    {{$t('teams.details')}}
+                                                    {{ $t('teams.details') }}
                                                 </button>
                                             </div>
                                         </div>
@@ -108,11 +114,13 @@
                                                         <div class="ml-4 mr-auto">
                                                             <a href="" class="font-medium">{{ member.name + ' ' + member.lastname }}</a>
                                                             <div class="text-gray-600 mr-5 sm:mr-5" v-if="member.companies.length != 0">
-                                                                {{member.companies[0].company_name}}
+                                                                {{ member.companies[0].company_name }}
                                                             </div>
                                                         </div>
                                                         <div class="flex justify-center items-center" v-if="member.id != user.id">
-                                                            <a :disabled="isDisabled" @click.prevent="del(member.id,team.id, index)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon> Delete </a>
+                                                            <a :disabled="isDisabled" @click.prevent="del(member.id,team.id, index)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal">
+                                                                <TrashIcon></TrashIcon>
+                                                                Delete </a>
                                                         </div>
                                                         <div class="font-medium text-gray-700 dark:text-gray-600">
                                                         </div>
@@ -125,17 +133,18 @@
                             </div>
                         </div>
                     </div>
-                    <!-- BEGIN: Users Layout -->
-
                 </div>
+                <!-- BEGIN: Users Layout -->
+
             </div>
-            <!-- BEGIN: Users Layout -->
         </div>
+        <!-- BEGIN: Users Layout -->
+    </div>
     <Modal :show="show" @closed="modalClosed">
-        <h3 class="intro-y text-lg font-medium mt-5">{{$t('teams.addMember')}}</h3>
+        <h3 class="intro-y text-lg font-medium mt-5">{{ $t('teams.addMember') }}</h3>
         <div class="intro-y box p-5 mt-12 sm:mt-5">
             <div>
-                {{$t('teams.description')}}
+                {{ $t('teams.description') }}
             </div>
         </div>
         <div class="intro-y box p-5 mt-12 sm:mt-5">
@@ -177,9 +186,8 @@ export default {
         const temporary_team_id = ref(null);
 
 
-
         const teamsObject = computed(() => {
-            if(props.who==='challenge') {
+            if (props.who === 'challenge') {
                 return props.challenge.teams;
             } else {
                 return props.solution.teams;
@@ -189,12 +197,12 @@ export default {
             teams.value = GetTeams();
         }
         const showAddToTeamModal = (id) => {
-            if(temporary_team_id == null || temporary_team_id === id) {
+            if (temporary_team_id == null || temporary_team_id === id) {
                 show.value = !show.value;
             } else {
                 show.value = true;
             }
-            temporary_team_id.value =  id;
+            temporary_team_id.value = id;
         }
 
         const modalClosed = () => {
@@ -202,60 +210,57 @@ export default {
             temporary_team_id.value = null;
         }
 
-        const del = async (member_id,team_id, index) => {
+        const del = async (member_id, team_id, index) => {
             axios.post('api/teams/user/member/delete', {member_id: member_id, team_id: team_id})
                 .then(response => {
                     // console.log(response.data)
                     if (response.data.success) {
                         isDisabled.value = true;
                         toast.success(response.data.message);
-                        teamsObject[team_id].users.splice(index,1);
-                        setTimeout(() =>{
+                        teamsObject[team_id].users.splice(index, 1);
+                        setTimeout(() => {
                             isDisabled.value = false;
                         }, 2000);
 
                     } else {
                         isDisabled.value = true;
                         toast.error(response.data.message);
-                        setTimeout(() =>{
+                        setTimeout(() => {
                             isDisabled.value = false;
                         }, 2000);
                     }
-                    setTimeout(() =>{
+                    setTimeout(() => {
                         isDisabled.value = false;
                     }, 2000);
                 })
         }
 
         const addObjectTeam = async () => {
-            if(new_team_name.value === '') {
+            if (new_team_name.value === '') {
                 toast.error('Nazwa nie może być pusta');
-                isDisabled.value=true;
+                isDisabled.value = true;
             } else if (new_team_name.value.length < 3) {
                 toast.error('Nazwa nie może mieć mniej niż 3 znaki');
-                isDisabled.value=true;
+                isDisabled.value = true;
             } else {
                 let id;
-                if(props.who === 'challenge')
-                {
+                if (props.who === 'challenge') {
                     id = props.challenge.id;
-                }
-                else
-                {
+                } else {
                     id = props.solution.id;
                 }
                 await AddObjectTeam(props.who, new_team_name.value, id, (res) => {
                     teamsObject.value.push(res);
                 })
-                isDisabled.value=true;
+                isDisabled.value = true;
             }
-            setTimeout(()=>{
-                isDisabled.value=false;
-            },5000);
+            setTimeout(() => {
+                isDisabled.value = false;
+            }, 5000);
         }
 
         const addMember = async () => {
-            if(new_team_member_email.value === '') {
+            if (new_team_member_email.value === '') {
                 isDisabled.value = true;
                 toast.error('Email nie może być pusty');
             } else if (new_team_member_email.value.length < 3) {
@@ -270,7 +275,7 @@ export default {
                 }, 1000);
                 toast.success('Success!')
             }
-            setTimeout(() =>{
+            setTimeout(() => {
                 isDisabled.value = false;
             }, 2000);
         }
