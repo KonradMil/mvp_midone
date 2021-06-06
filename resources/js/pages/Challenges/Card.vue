ea0b1251faf0ac098f476d25e4357d1f36d6422c
konradmilewski <konrad@kmilewski.eu>
2021-06-06 18:34:01 +0200

    ff

--- resources/js/pages/Challenges/Card.vue
+++ resources/js/pages/Challenges/Card.vue
@@ -61,7 +61,7 @@
                     </div>
                     <div class="p-5 border-t border-gray-200 dark:border-dark-5" v-if="(challenge.author_id == user.id)">
                         <a class="flex items-center" href=""
-                           @click.prevent="activeTab = 'zespoly'"
+                           @click.prevent="activeTab = 'teams'"
                            :class="(activeTab == 'teams')? ' text-theme-1 dark:text-theme-10 font-medium' : ''">
                             <ActivityIcon class="w-4 h-4 mr-2"/>
                             Zespoły
