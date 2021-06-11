a248127ae5532acb67bb690825fe519acad5b83c
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-11 13:51:37 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -25,11 +25,19 @@ class OfferController extends Controller
     public function getAllChallengeOffers(Challenge $challenge)
     {
         $offers = Offer::where('challenge_id', '=', $challenge->id)->with('solution')->get();
+        $array = [];
+
+        foreach($offers as $offer) {
+            $solution = Solution::find($offer->solution_id);
+            if($solution->rejected == true){
+                 $array[] = $offer;
+            }
+        }
 
         return response()->json([
             'success' => true,
             'message' => 'Pobrano oferty poprawnie.',
-            'payload' => $offers
+            'payload' => $array
         ]);
     }
 
