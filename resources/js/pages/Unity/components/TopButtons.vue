cc81c13af875b8e3d2c1ce3ffd28d257b8789859
konradmilewski <konrad@kmilewski.eu>
2021-06-15 16:07:31 +0200

    ff

--- resources/js/pages/Unity/components/TopButtons.vue
+++ resources/js/pages/Unity/components/TopButtons.vue
@@ -15,14 +15,14 @@
                     <UnityButton :tooltip="''" :alttext="''" :path="'/s3/builder_icons/logout_simple.png'" :action="'logout'" position="topbuttonclick"/>
 <!--                    <UnityDropdown v-if="icon.type === 'dropdown'" :alttext="icon.alttext" :path="icon.src" :action="index" position="topbuttonclick" :options="icon.options"></UnityDropdown>-->
                 </div>
-                <div :key="'topIcon_eye'" class="top-i w-30 pl-6 unity-button">
+                <div :key="'topIcon_eye'" class="top-i w-30 pl-6 unity-button" v-if="allowedEdit">
 <!--                    <UnityButton  v-if="icon.type === 'button'" :tooltip="icon.tooltip" :alttext="icon.alttext" :path="icon.src" :action="index" position="topbuttonclick"/>-->
                     <UnityDropdown :alttext="''" :path="'/s3/builder_icons/eye_simple3.png'" :action="'eye'" position="topbuttonclick" :options="icons.right['eye'].options"></UnityDropdown>
                 </div>
                 <div :key="'topIcon_save'" class="top-i w-30 pl-6 unity-button" v-if="allowedEdit">
                     <UnityButton  :tooltip="''" :alttext="''" :path="'/s3/builder_icons/save_simple.png'" :action="'save'" position="topbuttonclick"/>
                 </div>
-                <div :key="'topIcon_help'" class="top-i w-30 pl-6 unity-button">
+                <div :key="'topIcon_help'" class="top-i w-30 pl-6 unity-button" v-if="allowedEdit">
                     <UnityButton :alttext="''" :path="'/s3/builder_icons/help_simple.png'" :action="'help'" position="topbuttonclick" ></UnityButton>
                 </div>
             </div>
