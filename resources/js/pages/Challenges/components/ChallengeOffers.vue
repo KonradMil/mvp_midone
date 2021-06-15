739a18b6ab7e4faa96918c8b80baed288e22541f
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-15 16:32:03 +0200

    cc

--- resources/js/pages/Challenges/components/ChallengeOffers.vue
+++ resources/js/pages/Challenges/components/ChallengeOffers.vue
@@ -211,9 +211,9 @@ export default {
                         offer.selected = 0;
                         offer.solution.selected_offer_id = 0;
                         props.challenge.selected_offer_id = 0;
-                        const offerIndex = offers.value.findIndex(a => a.id === offer.id);
+                        const offerIndex = offers.value.list.findIndex(a => a.id === offer.id);
                         if (offerIndex !== -1) {
-                            offers.value.splice(offerIndex,1);
+                            offers.value.list.splice(offerIndex,1);
                         }
                     } else {
                         // toast.error(response.data.message);
