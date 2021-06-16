1703028c3e7df15f12fa31cb77349a8925598d91
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-16 13:06:41 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -73,6 +73,26 @@ class OfferController extends Controller
 
         if($offer == NULL) {
             $check = new Offer();
+            $check->challenge_id = $request->challenge_id;
+            $check->solution_id = $request->solution_id;
+            $check->installer_id = Auth::user()->id;
+            $check->price_of_delivery = $request->price_of_delivery;
+            $check->weeks_to_start = $request->weeks_to_start;
+            $check->time_to_start = $request->time_to_start;
+            $check->time_to_fix = $request->time_to_fix;
+            $check->advance_upon_start = $request->advance_upon_start;
+            $check->advance_upon_delivery = $request->advance_upon_delivery;
+            $check->advance_upon_agreement = $request->advance_upon_agreement;
+            $check->years_of_guarantee = $request->years_of_guarantee;
+            $check->service_support_scope = $request->service_support_scope;
+            $check->maintenance_frequency = $request->maintenance_frequency;
+            $check->price_of_maintenance = $request->price_of_maintenance;
+            $check->reaction_time = $request->reaction_time;
+            $check->intervention_price = $request->intervention_price;
+            $check->work_hour_price = $request->work_hour_price;
+            $check->period_of_support = $request->period_of_support;
+            $check->offer_expires_in = $request->offer_expires_in;
+            $check->save();
         }
         else {
             $offer->price_of_delivery = $request->price_of_delivery;
@@ -91,35 +111,15 @@ class OfferController extends Controller
             $offer->work_hour_price = $request->work_hour_price;
             $offer->period_of_support = $request->period_of_support;
             $offer->offer_expires_in = $request->offer_expires_in;
+            $offer->save();
         }
 
 //        $check->fill($request->input());
-        $check->challenge_id = $request->challenge_id;
-        $check->solution_id = $request->solution_id;
-        $check->installer_id = Auth::user()->id;
-        $check->price_of_delivery = $request->price_of_delivery;
-        $check->weeks_to_start = $request->weeks_to_start;
-        $check->time_to_start = $request->time_to_start;
-        $check->time_to_fix = $request->time_to_fix;
-        $check->advance_upon_start = $request->advance_upon_start;
-        $check->advance_upon_delivery = $request->advance_upon_delivery;
-        $check->advance_upon_agreement = $request->advance_upon_agreement;
-        $check->years_of_guarantee = $request->years_of_guarantee;
-        $check->service_support_scope = $request->service_support_scope;
-        $check->maintenance_frequency = $request->maintenance_frequency;
-        $check->price_of_maintenance = $request->price_of_maintenance;
-        $check->reaction_time = $request->reaction_time;
-        $check->intervention_price = $request->intervention_price;
-        $check->work_hour_price = $request->work_hour_price;
-        $check->period_of_support = $request->period_of_support;
-        $check->offer_expires_in = $request->offer_expires_in;
-
-
 //        $solution = Solution::find($request->input('solution_id'));
 //        $check->challenge_id = $solution->challenge_id;
 
-        $check->save();
-        $offer->save();
+
+
 
         if($check == NULL){
             return response()->json([
