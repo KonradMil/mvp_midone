c72718bb39fbf7a18536a340b98e4e0130df6e9a
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-16 16:13:25 +0200

    cc

--- resources/js/pages/Challenges/components/OfferAdd.vue
+++ resources/js/pages/Challenges/components/OfferAdd.vue
@@ -258,8 +258,11 @@ export default {
         });
 
 
-        const getOffer = () => {
-            let id = props.edit_offer_id;
+        const getOffer = (val = 0) => {
+            let id = props.offer_id;
+            if (val != 0) {
+                id = val;
+            }
             console.log(id + '-> edit offer id');
             axios.post('/api/offer/get', {id: id})
                 .then(response => {
