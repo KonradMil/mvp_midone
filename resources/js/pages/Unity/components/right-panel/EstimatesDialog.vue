56a7fa4c74a24845eab103c24a9d83055d446910
konradmilewski <konrad@kmilewski.eu>
2021-06-17 21:12:27 +0200

    ff

--- resources/js/pages/Unity/components/right-panel/EstimatesDialog.vue
+++ resources/js/pages/Unity/components/right-panel/EstimatesDialog.vue
@@ -57,7 +57,7 @@ export default {
                                 return false;
                             } else {
                                 partsAr.value.push({
-
+                                
                                 });
                                 return true;
                             }
