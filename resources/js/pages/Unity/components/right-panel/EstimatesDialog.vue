c5ce47dc8a19bc6cb1c54428eb3203a087ae9c0b
konradmilewski <konrad@kmilewski.eu>
2021-06-17 20:29:08 +0200

    ff

--- resources/js/pages/Unity/components/right-panel/EstimatesDialog.vue
+++ resources/js/pages/Unity/components/right-panel/EstimatesDialog.vue
@@ -46,16 +46,19 @@ export default {
 
         const finalPartsList = computed(() => {
                 let c = props.parts;
+                let tempChallenge = JSON.parse(challenge.value.save_json);
                 if(c.length != undefined) {
                     console.log(JSON.parse(challenge.value.save_json).parts);
-                JSON.parse(challenge.value.save_json).parts.forEach((obj, indx) => {
+                    tempChallenge.parts.forEach((obj, indx) => {
                     console.log(indx);
-                    c.every((obj2) => {
+                    c.forEach((obj2) => {
                         console.log(obj.model.model_name);
                         console.log(obj2.model_name);
                         if (obj.model.model_name == obj2.model_name) {
                             c.splice(indx, 1);
                             return false;
+                        } else {
+                            return true;
                         }
                     });
             });
