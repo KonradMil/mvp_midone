2539edbc3b0659b806ee551e105cf314c71534e6
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-21 17:31:55 +0200

    cc

--- app/Http/Controllers/Crud/ChallengeController.php
+++ app/Http/Controllers/Crud/ChallengeController.php
@@ -119,96 +119,95 @@ class ChallengeController extends Controller
     {
         $input = $request->input();
         $query = Challenge::query();
-        $check = false;
-        if (Auth::user()->type == 'integrator') {
-            $challengesProject = Challenge::where('stage', '=', 3)->where('status', '=', 1)->get();
-            foreach ($challengesProject as $challenge) {
-                $offer = Offer::find($challengesProject->selected_offer_id);
-                $solution = Solution::find($offer->solution_id);
+        $challengesProject = Challenge::where('stage', '=', 3)->where('status', '=', 1)->get();
+        if($challengesProject != NULL){
 
-                foreach ($solution->teams as $team) {
+            if (Auth::user()->type == 'integrator' && $challengesProject != NULL) {
+                foreach ($challengesProject as $challenge) {
+                    $offer = Offer::find($challengesProject->selected_offer_id);
+                    $solution = Solution::find($offer->solution_id);
+
+                    foreach ($solution->teams as $team) {
+                        foreach (Auth::user()->teams as $t) {
+                            if ($t->id == $team->id) {
+                                $query->where('stage', '=', 3)->where('status', '=', 1);
+
+                            }
+                        }
+                    }
+                }
+
+            } else if (Auth::user()->type == 'investor') {
+                foreach ($challengesProject->teams as $team) {
                     foreach (Auth::user()->teams as $t) {
                         if ($t->id == $team->id) {
-                            $query->where('stage', '=', 3)->where('status', '=', 1);
-                            $check = true;
+                            $query->where('author_id', '=', Auth::user()->id)->where('stage', '=', 3);
                         }
                     }
                 }
             }
 
-        } else if (Auth::user()->type == 'investor') {
-            $challengesProject = Challenge::where('stage', '=', 3)->where('status', '=', 1)->get();
-            foreach ($challengesProject->teams as $team) {
-                foreach (Auth::user()->teams as $t) {
-                    if ($t->id == $team->id) {
-                        $query->where('author_id', '=', Auth::user()->id)->where('stage', '=', 3);
-                        $check = true;
-                    }
+            if (isset($input->status)) {
+                $query->where('status', '=', $input->status);
+            }
+            if (isset($input->type)) {
+                $query->where('type', '=', $input->type);
+            }
+            if (isset($input->rating)) {
+                $query->whereIn('rating', [($input->rating - 0.5), $input->rating, ($input->rating + 0.5)]);
+            }
+            if (isset($input->favourite)) {
+                $query->where('favourite', '=', 1);
+            }
+
+            $challenges = $query->with(['comments.commentator', 'technicalDetails', 'financial_before'])->get();
+
+            $ars = [];
+
+            $ts = Auth::user()->teams;
+
+            foreach ($ts as $tt) {
+                array_push($ars, $tt->id);
+            }
+
+            $c = Challenge::whereHas('teams', function ($query) use ($ars) {
+                $query->whereIn('teams.id', $ars);
+            })->orderBy('created_at', 'DESC')->get();
+
+
+            $merged = $challenges->merge($c);
+
+            foreach ($challenges as $challenge) {
+
+                if (Auth::user()->viaLoveReacter()->hasReactedTo($challenge, 'Like')) {
+                    $challenge->liked = true;
+                } else {
+                    $challenge->liked = false;
+                }
+                $challenge->comments_count = $challenge->comments()->count();
+                $challenge->likes = $challenge->viaLoveReactant()->getReactionCounterOfType('Like')->getCount();
+
+                if (Auth::user()->viaLoveReacter()->hasReactedTo($challenge, 'Follow', 1)) {
+                    $challenge->followed = true;
+                } else {
+                    $challenge->followed = false;
                 }
             }
+
+            return response()->json([
+                    'success' => true,
+                    'message' => 'Pobrano poprawnie.',
+                    'payload' => $merged->all()
+                ]);
+
+        } else {
+            return response()->json([
+                'success' => true,
+                'message' => 'Brak projektów.',
+                'payload' => ''
+            ]);
         }
 
-        if (isset($input->status)) {
-            $query->where('status', '=', $input->status);
-        }
-        if (isset($input->type)) {
-            $query->where('type', '=', $input->type);
-        }
-        if (isset($input->rating)) {
-            $query->whereIn('rating', [($input->rating - 0.5), $input->rating, ($input->rating + 0.5)]);
-        }
-        if (isset($input->favourite)) {
-            $query->where('favourite', '=', 1);
-        }
-
-        $challenges = $query->with(['comments.commentator', 'technicalDetails', 'financial_before'])->get();
-
-        $ars = [];
-
-        $ts = Auth::user()->teams;
-
-        foreach ($ts as $tt) {
-            array_push($ars, $tt->id);
-        }
-
-        $c = Challenge::whereHas('teams', function ($query) use ($ars) {
-            $query->whereIn('teams.id', $ars);
-        })->orderBy('created_at', 'DESC')->get();
-
-
-        $merged = $challenges->merge($c);
-
-        foreach ($challenges as $challenge) {
-
-            if (Auth::user()->viaLoveReacter()->hasReactedTo($challenge, 'Like')) {
-                $challenge->liked = true;
-            } else {
-                $challenge->liked = false;
-            }
-            $challenge->comments_count = $challenge->comments()->count();
-            $challenge->likes = $challenge->viaLoveReactant()->getReactionCounterOfType('Like')->getCount();
-
-            if (Auth::user()->viaLoveReacter()->hasReactedTo($challenge, 'Follow', 1)) {
-                $challenge->followed = true;
-            } else {
-                $challenge->followed = false;
-            }
-        }
-
-
-    if ($check == true) {
-        return response()->json([
-            'success' => true,
-            'message' => 'Pobrano poprawnie.',
-            'payload' => $merged->all()
-        ]);
-    } else {
-        return response()->json([
-            'success' => true,
-            'message' => 'Pobrano poprawnie.',
-            'payload' => ''
-        ]);
-     }
 
     }
 
