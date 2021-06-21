b0499887de43ae7f92187b48197c468b3af4dec6
konradmilewski <konrad@kmilewski.eu>
2021-06-21 14:14:46 +0200

    ff

--- app/Models/OldChallenge.php
+++ app/Models/OldChallenge.php
@@ -24,7 +24,7 @@ class OldChallenge extends Model
 
     public function teams()
     {
-        return $this->belongsToMany(OldTeam::class, 'teams_challenges', 'team_id', 'challenge_id');
+        return $this->belongsToMany(OldTeam::class, 'teams_challenges', 'challenge_id', 'team_id');
     }
 
     public function financial_before()
