411cd33cd3e139c54521b153efb9382c443e1548
konradmilewski <konrad@kmilewski.eu>
2021-06-21 15:03:46 +0200

    ff

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\S3Controller;
 use Illuminate\Support\Facades\Route;
 Route::get('s3/{path}', [S3Controller::class, 'reRoute']) ->where('path', '(.*)');;
+Route::get('models/{path}', [S3Controller::class, 'reRoute']) ->where('path', '(.*)');;
 Route::get('studio/s3/{path}', [S3Controller::class, 'reRoute']) ->where('path', '(.*)');;
 Route::get('{any}', function () {
     return view('app');
