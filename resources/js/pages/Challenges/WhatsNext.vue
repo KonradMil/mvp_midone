16c5b8bef8923da4ae6f5a1f3875249a80816c56
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-22 13:54:32 +0200

    cc

--- resources/js/pages/Challenges/WhatsNext.vue
+++ resources/js/pages/Challenges/WhatsNext.vue
@@ -82,7 +82,7 @@ name: "WhatsNext",
                     console.log('HERE');
                     text.value = 'Na tym etapie Inwestor oczekuje na rozwiązania technologiczne. Przygotuj koncepcję swojego rozwiązania.';
                     action.value = {redirect: ''}
-                } else if(props.challenge.solutions.length > 0 && props.challenge.stage == 1) {
+                } else if(props.solutions.length > 0 && props.challenge.stage == 1) {
                     text.value = 'Po opublikowaniu rozwiązania będzie ono widoczne dla Inwestora.';
                     action.value = {redirect: ''}
                 }else if(props.challenge.stage == 2) {
