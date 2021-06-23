6b619405ff9fde6e3c704b88b815bc16ad7ad1d0
konradmilewski <konrad@kmilewski.eu>
2021-06-23 12:47:09 +0200

    ff

--- app/Http/Controllers/OldImportController.php
+++ app/Http/Controllers/OldImportController.php
@@ -93,8 +93,8 @@ class OldImportController extends Controller
             $ou = OldUser::where('id', '=', $team->author_id)->first();
             $nt = Team::where('name', '=', $team->name)->first();
             $nu = User::where('email', '=', $ou->email)->first();
-            $nu->teams()->attach($nt);
-            $nt->author_id = $nu->id;
+//            $nu->teams()->attach($nt);
+            $nt->owner_id = $nu->id;
             $nt->save();
         }
 
