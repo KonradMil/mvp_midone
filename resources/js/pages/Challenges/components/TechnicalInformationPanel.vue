d72068b62cad31bfdd98fb3c063d5b4d302d1b2a
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-28 11:55:49 +0200

    cc

--- resources/js/pages/Challenges/components/TechnicalInformationPanel.vue
+++ resources/js/pages/Challenges/components/TechnicalInformationPanel.vue
@@ -6,7 +6,7 @@
                 <div
                     class="flex items-center px-5 py-3 border-b border-gray-200 dark:border-dark-5"
                 >
-                    <h2 class="font-medium text-base mr-auto"> Szczegóły techniczne</h2>
+                    <h2 class="font-medium text-base mr-auto">{{$t('challengesMain.technicalDetails')}}</h2>
                 </div>
                 <div class="px-5 py-5">
                     <div
@@ -18,61 +18,61 @@
                     >
                         <div class="flex items-center">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Maksymalna waga detalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.maxWeight')}}:</span>
                                 <div class=" dark:text-theme-10 text-theme-1"> {{ details['select_detail_weight'][challenge.technical_details.detail_weight].name }}</div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Jakość chwytania detalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.quality')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_pick_quality'][challenge.technical_details.pick_quality].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Materiał detalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.detail')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_detail_material'][challenge.technical_details.detail_material].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Wielkość detalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.size')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_detail_size'][challenge.technical_details.detail_size].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Sposób pobierania metalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.way')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_detail_pick'][challenge.technical_details.detail_pick].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Czy detal jest pozycjonowany do pobrania?</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.position')}}</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_detail_position'][challenge.technical_details.detail_position].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Odległość pomiędzy miejscem pobrania i odkładania detalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.distance')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_detail_range'][challenge.technical_details.detail_range].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Miejsce odłożenia detalu:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.place')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_detail_destination'][challenge.technical_details.detail_destination].name }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Ilość obsługiwanych stanowisk/linii:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.numberSupported')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{challenge.technical_details.number_of_lines }} </div>
                             </div>
                         </div>
                         <div class="flex items-center mt-5">
                             <div class="border-l-2 border-theme-1 pl-4">
-                                <span class="font-medium text-gray-600">Liczba zmian:</span>
+                                <span class="font-medium text-gray-600">{{$t('challengesNew.changeNumber')}}:</span>
                                 <div class="dark:text-theme-10 text-theme-1"> {{ details['select_work_shifts'][challenge.technical_details.work_shifts].name }} </div>
                             </div>
                         </div>
@@ -85,7 +85,7 @@
                 <div
                     class="flex items-center px-5 py-3 border-b border-gray-200 dark:border-dark-5"
                 >
-                    <h2 class="font-medium text-base mr-auto"> Szczegóły finansowe</h2>
+                    <h2 class="font-medium text-base mr-auto">{{$t('challengesMain.financialDetails')}}</h2>
                 </div>
                 <div class="px-5 py-5">
                     <div
