aa4a744146a8f201b73d362cd20d2f591ca08be3
Lukaszewski <lmioskowski1999@gmail.com>
2021-06-29 10:19:06 +0200

    cc

--- resources/js/pages/Challenges/components/BasicInformationPanel.vue
+++ resources/js/pages/Challenges/components/BasicInformationPanel.vue
@@ -66,7 +66,7 @@
                         }" class="form-control"/>
                         <span v-if="!inTeam"> {{ $dayjs(challenge.offer_deadline).format('DD.MM.YYYY') }} </span>
                     </div>
-                    <button v-if="inTeam" class="btn btn-secondary ml-auto my-1" @click="saveDate">
+                    <button v-if="inTeam && challenge.stage < 3" class="btn btn-secondary ml-auto my-1" @click="saveDate">
                         {{$t('challengesMain.changeDates')}}
                     </button>
                     <div class="flex items-center my-5">
