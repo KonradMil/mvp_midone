2c887fd73a3ef5934c9b71a4476ae7cf5a8ebbed
konradmilewski <konrad@kmilewski.eu>
2021-07-06 14:39:28 +0200

    fix

--- resources/js/pages/ForgotPassword.vue
+++ resources/js/pages/ForgotPassword.vue
@@ -37,7 +37,7 @@
                         <h2
                             class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left"
                         >
-                            {{$t('register.signup')}}
+                            Resetuj hasło
                         </h2>
                         <div
                             class="intro-x mt-2 text-gray-500 dark:text-gray-500 xl:hidden text-center"
@@ -109,7 +109,7 @@
                             <button type="submit"
                                 class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top"
                             >
-                                {{$t('register.signup')}}
+                                Resetuj
                             </button>
                             <button    @click="$router.push('login')"
                                 class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top"
