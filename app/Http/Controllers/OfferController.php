416735df1742be3bcaad96503ab64c087a9d8919
konradmilewski <konrad@kmilewski.eu>
2021-07-06 13:31:50 +0200

    fix

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -29,7 +29,7 @@ class OfferController extends Controller
         }else if($option === 'Czas realizacji uruchomienia u klienta'){
             $offers = $challenge->offers()->orderBy('time_to_start', 'DESC')->with('solution')->get();
         }else if($option === 'NPV'){
-            $offers = $challenge->offers()->join('solutions as so', 'so.id', '=', 'offers.solution_id')->join('financial_analyses as fa', 'fa.solution_id', '=', 'solutions.id')->with('solution', 'solution.financial_analyses')->orderBy('fa.npv', 'DESC')->get();
+            $offers = $challenge->offers()->join('solutions as so', 'solutions.id', '=', 'offers.solution_id')->join('financial_analyses as fa', 'fa.solution_id', '=', 'so.id')->with('solution', 'solution.financial_analyses')->orderBy('fa.npv', 'DESC')->get();
 //        $solutions = $challenge->solutions()->with('financial_analyses')->orderBy('financial_analyses.npv', 'DESC')->get();
 //        foreach($solutions as $solution){
 ////            $array[] = $solution->offers;
