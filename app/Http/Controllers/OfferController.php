4b601c8eea4c42711856572a2e5d1294f34e568c
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-07 13:15:32 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -150,7 +150,7 @@ class OfferController extends Controller
         } else {
             $check = new Offer();
 
-            $j = json_decode($request->data['solution_robots'], true);
+            $j = json_decode($request->data['solution_robots']['data'], true);
             $check->robots = json_encode($j);
             $check->challenge_id = $request->challenge_id;
             $check->solution_id = $request->solution_id;
