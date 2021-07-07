48ba9daa83da396a9853b75e9bb40c9825505d3e
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-07 11:14:09 +0200

    cc

--- resources/js/pages/Challenges/components/OfferAdd.vue
+++ resources/js/pages/Challenges/components/OfferAdd.vue
@@ -192,7 +192,8 @@
                             Okres gwarancji robota {{obj.name}}
                         </label>
                         <input type="number" class="form-control" v-model="guarantee_period"/>
-                            <a class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"><Edit2Icon class="w-4 h-4 mr-2"/> Edytuj </a>
+                            <a class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal">
+                                <Edit2Icon class="w-4 h-4 mr-2"/> Zapisz </a>
                     </div>
                     <template v-for="(obj, index) in Robots">
                         <div class="intro-y col-span-12 sm:col-span-6 mt-2" >
