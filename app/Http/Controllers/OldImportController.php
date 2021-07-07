33c5e853a07de50935ce98c57afbccf0809d0bd1
konradmilewski <konrad@kmilewski.eu>
2021-07-07 12:35:38 +0200

    fix

--- app/Http/Controllers/OldImportController.php
+++ app/Http/Controllers/OldImportController.php
@@ -25,7 +25,7 @@ class OldImportController extends Controller
     {
 
         $challenges = OldChallenge::get();
-        dump($challenges);
+        dd($challenges);
         foreach ($challenges as $challenge) {
             $nc = Challenge::where('name', '=', $challenge->name)->first();
             dump($challenge->files);
