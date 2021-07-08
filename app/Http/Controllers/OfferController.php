7ac583148c6fd4cbdfda3080a480a65d9655e542
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-08 23:06:46 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -388,7 +388,7 @@ class OfferController extends Controller
         $offer->rejected = true;
         $offer->selected = false;
         $challenge = Challenge::find($offer->challenge_id);
-        $challenge->dissociate($offer);
+        $challenge->offers()->dissociate($offer);
         if($challenge->selected_offer_id == $offer->id){
             $challenge->selected_offer_id = 0;
         }
