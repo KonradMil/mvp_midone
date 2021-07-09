2d063ddc1290a160e03eaca86c9f1627e9e41185
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-09 12:46:37 +0200

    cc

--- app/Http/Controllers/Crud/SolutionController.php
+++ app/Http/Controllers/Crud/SolutionController.php
@@ -651,15 +651,17 @@ class SolutionController extends Controller
         $solution->status = 0;
         $solution->screenshot_path = 'screenshots/dbr_placeholder.jpeg';
         $solution->save();
-        $estimate = new Estimate();
-        $estimate->solution_id = $solution->id;
-        $estimate->save();
-        $financial_analyses = new FinancialAnalysis();
-        $financial_analyses->solution_id = $solution->id;
-        $financial_analyses->save();
-        $operational_analyses = new OperationalAnalysis();
-        $operational_analyses->solution_id = $solution->id;
-        $operational_analyses->save();
+
+//        $estimate = new Estimate();
+//        $estimate->solution_id = $solution->id;
+//        $estimate->save();
+//        $financial_analyses = new FinancialAnalysis();
+//        $financial_analyses->solution_id = $solution->id;
+//        $financial_analyses->save();
+//        $operational_analyses = new OperationalAnalysis();
+//        $operational_analyses->solution_id = $solution->id;
+//        $operational_analyses->save();
+
 //        $financial->days = $request -> days;
 //        $financial->shifts = $request -> shifts;
 //        $financial->shift_time = $request -> shift_time;
