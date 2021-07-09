e9e3f9b5b524b201220800f3de474d8dc9243754
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-09 09:49:31 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -150,6 +150,8 @@ class OfferController extends Controller
             $offers = $challenge->offers()->orderBy('avg_guarantee', 'DESC')->with('solution')->get();
         }else if($option === 'Okres zwrotu inwestycji'){
             $offers = $challenge->offers()->join('solutions as so', 'so.id', '=', 'offers.solution_id')->join('financial_analyses as fa', 'fa.solution_id', '=', 'so.id')->orderBy('fa.simple_payback', 'DESC')->select('offers.*')->with('solution', 'solution.financial_analyses')->get();
+        }else if($option === 'Ranking'){
+            $offers = $challenge->offers()->orderBy('sum', 'DESC')->with('solution')->get();
         }
 
         return response()->json([
