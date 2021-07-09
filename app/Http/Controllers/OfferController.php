60b8edd6034a2e1d074e98e34e791e59e9e1de49
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-09 11:14:41 +0200

    cc

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -390,8 +390,7 @@ class OfferController extends Controller
         $offer->rejected = true;
         $offer->selected = false;
         $challenge = Challenge::find($offer->challenge_id);
-
-//        $challenge->offers()->detach($offer);
+        $challenge->offers()->detach($offer);
 
         if($challenge->selected_offer_id == $offer->id){
             $challenge->selected_offer_id = 0;
