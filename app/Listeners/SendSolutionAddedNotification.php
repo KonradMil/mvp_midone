8d2a632dc8b1292f876dfbc793ac6e344bc7583c
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-14 13:30:14 +0200

    cc

---
+++ app/Listeners/SendSolutionAddedNotification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SolutionPublished;
+use App\Models\Challenges\Challenge;
+use App\Models\Solutions\Solution;
+use App\Models\User;
+use App\Notifications\ChallengePublishedNotification;
+use App\Notifications\OfferPublishedNotification;
+use App\Notifications\SolutionAddedNotification;
+use App\Notifications\SolutionPublishedNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendSolutionAddedNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  SolutionPublished  $event
+     * @return void
+     */
+    public function handle(SolutionPublished $event)
+    {
+//            $user = User::find($event->subject->author_id);
+        $user = $event->subject->author;
+        $challenge = Challenge::find($event->subject->challenge_id);
+        $user->notify(new SolutionAddedNotification($challenge, $event->subject));
+    }
+}
