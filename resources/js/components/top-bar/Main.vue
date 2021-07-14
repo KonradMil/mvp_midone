dc6a035f06ae5f96d9145a8283fe4e0e32766d02
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-14 12:43:25 +0200

    cc

--- resources/js/components/top-bar/Main.vue
+++ resources/js/components/top-bar/Main.vue
@@ -78,7 +78,7 @@
                         class="cursor-pointer relative flex items-center"
                         :class="{ 'mt-5': index }"
                         @click="goTo(notification.data.name,notification.id,notification.data.params,notification.data.id)">
-                        <a v-if="notification.read_at !== null" class="flex items-center text-theme-6 pr-4" @click.prevent=delNotifi(notification.id) href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon></a>
+                        <a v-if="notification.read_at !== null" class="flex items-center text-theme-6 pr-4" @click.prevent=delNotifi(notification.id,index) href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon></a>
                         <div class="w-12 h-12 flex-none image-fit mr-1">
                             <Avatar :src="'/s3/avatars/' + notification.data.author.avatar"
                                     :username="notification.data.author.name + ' ' + notification.data.author.lastname"
@@ -331,12 +331,13 @@ export default defineComponent({
                 })
         }
 
-        const delNotifi = async (id) => {
+        const delNotifi = async (id,index) => {
             axios.post('/api/notifications/delete', {id: id})
                 .then(response => {
                     // console.log(response.data)
                     if (response.data.success) {
-                        notifications.value = response.data.payload
+                        // notifications.value = response.data.payload
+                        notifications.value.splice(index,1);
                         toast.success(response.data.message);
                     } else {
                     }
