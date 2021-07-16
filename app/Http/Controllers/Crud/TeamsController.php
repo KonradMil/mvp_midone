b3a523978cd5f055050a2b33aeebe4aca1c33d5b
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-16 15:40:36 +0200

    cc

--- app/Http/Controllers/Crud/TeamsController.php
+++ app/Http/Controllers/Crud/TeamsController.php
@@ -70,7 +70,7 @@ class TeamsController extends Controller
         }
 
         if($who == 'teams'){
-            $query = Auth::user()->teams()->with('users', 'users.companies')->get();
+            $query = Auth::user()->teams()->with('users', 'users.companies', 'users.publishChallenge')->get();
         }else if ((!empty($input->search)) && ($who != 'teams')) {
             $query = Auth::user()->teams()->where('name', 'LIKE', '%' . $input->search . '%')->whereNotIn('id', $array)->with('users', 'users.companies')->get();
 //            $queryForeign = Auth::user()->ownedTeams->where('name', 'LIKE', '%' . $input->search . '%')->get();
