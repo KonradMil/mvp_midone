526334fcf678e04fd0a198cd3628958acb94588d
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-16 15:49:45 +0200

    cc

--- resources/js/pages/Teams/Teams.vue
+++ resources/js/pages/Teams/Teams.vue
@@ -71,7 +71,7 @@
                                                 </div>
                                             </div>
                                             <div class="flex justify-center items-center" v-if="team.owner_id == user.id">
-                                                <button class="btn btn-outline-secondary py-1 px-2" @click="showDetails[team.id] = !showDetails[team.id]">
+                                                <button class="btn btn-outline-secondary py-1 px-2" @click="showPermissions[team.id] = !showPermissions[team.id]">
                                                     Uprawnienia
                                                 </button>
                                                 <a v-if="team.owner_id != member.id" :disabled="isDisabled" @click.prevent="del(member.id,team.id)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon> Delete </a>
