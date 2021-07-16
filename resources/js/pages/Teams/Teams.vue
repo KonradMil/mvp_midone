aa8fedf899218af9641fed5d079cdfb6cf773eb0
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-16 13:48:43 +0200

    cc

--- resources/js/pages/Teams/Teams.vue
+++ resources/js/pages/Teams/Teams.vue
@@ -72,6 +72,7 @@
                                             </div>
                                             <div class="flex justify-center items-center" v-if="team.owner_id == user.id">
                                                 <a v-if="team.owner_id != member.id" :disabled="isDisabled" @click.prevent="del(member.id,team.id)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon> Delete </a>
+                                                <a v-if="team.owner_id != member.id" :disabled="isDisabled" @click.prevent="del(member.id,team.id)" class="flex items-center text-theme-6" href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon></TrashIcon> Uprawnienia </a>
                                             </div>
                                             <div class="font-medium text-gray-700 dark:text-gray-600">
                                             </div>
