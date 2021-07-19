7c5dc1eef150a940d28462199549551f3f4d878f
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-19 13:47:41 +0200

    cc

--- resources/js/components/ModalPermission.vue
+++ resources/js/components/ModalPermission.vue
@@ -11,7 +11,6 @@
             <div
                 ref="modal-backdrop"
                 class="fixed z-50 inset-0 overflow-y-auto bg-black bg-opacity-50"
-                style="max-width: 300px;"
                 v-show="showModal"
             >
                 <div
