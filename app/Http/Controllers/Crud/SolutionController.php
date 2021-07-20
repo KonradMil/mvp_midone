5e94af40a5f7bbe45d98ce7f175d7db411778baf
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-20 11:58:33 +0200

    cc

--- app/Http/Controllers/Crud/SolutionController.php
+++ app/Http/Controllers/Crud/SolutionController.php
@@ -197,10 +197,12 @@ class SolutionController extends Controller
             $robots = [];
             if($save != NULL){
                 foreach($save as $key => $val){
-                    $model = UnityModel::find($key->model_name);
-                    $model->guarantee_period = 0;
-                    if($model->category == 1) {
-                        $robots[] = $model;
+                    if($key->count > 0){
+                        $model = UnityModel::find($key->model_name);
+                        $model->guarantee_period = 0;
+                        if($model->category == 1) {
+                            $robots[] = $model;
+                        }
                     }
                 }
             }
