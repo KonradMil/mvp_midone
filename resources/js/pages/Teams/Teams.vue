5941385ffa2899edc278b9eb5a82e8e524debcb1
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-26 13:38:41 +0200

    cc

--- resources/js/pages/Teams/Teams.vue
+++ resources/js/pages/Teams/Teams.vue
@@ -192,7 +192,7 @@
                         id="addSolutionOffer"
                         type="checkbox"
                         class="form-check-input border mr-2 ring-0"
-                        :checked="addSolutionOffer"/>
+                        v-model="addSolutionOffer"/>
                     <label class="cursor-pointer select-none" for="addSolutionOffer">{{ $t('global.addSolutionOffer') }}</label>
                 </div>
                 <div v-if="user.type === 'investor'" class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm pb-5 pt-2">
@@ -200,7 +200,7 @@
                         id="acceptChallengeSolution"
                         type="checkbox"
                         class="form-check-input border mr-2 ring-0"
-                        :checked="acceptChallengeSolution"/>
+                        v-model="acceptChallengeSolution"/>
                     <label class="cursor-pointer select-none" for="acceptChallengeSolution">{{ $t('global.acceptChallengeSolution') }}</label>
                 </div>
                 <div v-if="user.type === 'integrator'" class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm pb-5 pt-2">
@@ -208,7 +208,7 @@
                         id="addChallengeSolution"
                         type="checkbox"
                         class="form-check-input border mr-2 ring-0"
-                        :checked="addChallengeSolution"/>
+                        v-model="addChallengeSolution"/>
                     <label class="cursor-pointer select-none" for="acceptChallengeSolution">{{ $t('global.addChallengeSolution') }}</label>
                 </div>
 <!--                <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm pb-5">-->
