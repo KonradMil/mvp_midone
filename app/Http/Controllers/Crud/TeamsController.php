34542e44c73fe16ee3489153fbd78da028f36036
Lukaszewski <lmioskowski1999@gmail.com>
2021-07-27 10:48:26 +0200

    cc

--- app/Http/Controllers/Crud/TeamsController.php
+++ app/Http/Controllers/Crud/TeamsController.php
@@ -22,20 +22,23 @@ class TeamsController extends Controller
     {
         $team = Team::find($request->input('team_id'));
         $member = User::find($request->input('member_id'));
-        $team_user = TeamUser::where('user_id', '=', $member->id)->where('team_id', '=', $team->id)->first();
+//        $team_user = TeamUser::where('user_id', '=', $member->id)->where('team_id', '=', $team->id)->first();
 
+        $team_user = $member->teams()->where(['team_id'=>$team->id,'user_id'=>$member->id])->first(); //get the first record
 
+        $team_user->pivot->publishChallenge = 0; //change your col to a new value
 
-       if($team_user != NULL){
+        $team_user->pivot->save(); //save
+
+//       if($team_user != NULL){
 //            $team_user -> publishChallenge = $request->input('publishChallenge');
-            $team_user -> publishChallenge = 0;
-            $team_user -> publishSolution = $request->input('publishSolution');
-            $team_user -> acceptChallengeOffer = $request->input('acceptChallengeOffer');
-            $team_user -> addSolutionOffer = $request->input('addSolutionOffer');
-            $team_user -> acceptChallengeSolution = $request->input('acceptChallengeSolution');
-            $team_user -> addChallengeSolution = $request->input('addChallengeSolution');
-            $team_user->save();
-        }
+//            $team_user -> publishSolution = $request->input('publishSolution');
+//            $team_user -> acceptChallengeOffer = $request->input('acceptChallengeOffer');
+//            $team_user -> addSolutionOffer = $request->input('addSolutionOffer');
+//            $team_user -> acceptChallengeSolution = $request->input('acceptChallengeSolution');
+//            $team_user -> addChallengeSolution = $request->input('addChallengeSolution');
+//            $team_user->save();
+//        }
 
 //        $teams = $member->teams()->where('id', '=', $team->id);
         return response()->json([
