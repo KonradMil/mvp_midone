a252c210296654c4202426f03af3b264a7387874
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-03 14:39:15 +0200

    cc

--- resources/js/components/top-bar/Main.vue
+++ resources/js/components/top-bar/Main.vue
@@ -78,9 +78,8 @@
                     <div
                         v-for="(notification, index) in notificationsComp"
                         :key="'notification_' + index"
-                        class="cursor-pointer relative flex items-center"
+                        class="cursor-pointer relative flex items-center transform hover:-translate-x-10"
                         :class="{ 'mt-5': index }">
-                        <a v-if="notification.read_at !== null" class="flex items-center text-theme-6 pr-2" @click.prevent=delNotifi(notification.id,index) href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon style="width: 16px;"></TrashIcon></a>
                         <div class="w-12 h-12 flex-none image-fit mr-1">
                             <Avatar :src="'/s3/avatars/' + notification.data.author.avatar"
                                     :username="notification.data.author.name + ' ' + notification.data.author.lastname"
@@ -100,6 +99,7 @@
                                 {{ notification.data.message }}
                             </div>
                         </div>
+                        <a v-if="notification.read_at !== null" class="flex items-center text-theme-6 pr-2" @click.prevent=delNotifi(notification.id,index) href="javascript:;" data-toggle="modal" data-target="#delete-confirmation-modal"> <TrashIcon style="width: 16px;"></TrashIcon></a>
                     </div>
                     <div
                        v-if="notifications.length === 0"
