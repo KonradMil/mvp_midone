6add6752dc9bca3df5a1f0dc177f339dae060e60
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-03 12:41:02 +0200

    cc

--- resources/js/components/top-bar/Main.vue
+++ resources/js/components/top-bar/Main.vue
@@ -283,7 +283,7 @@ export default defineComponent({
         const getNotificationsRepositories = async () => {
             console.log(GetNotifications());
             // if(GetNotifications().list.)
-            notifications.value = GetNotifications();
+            notifications.value = await GetNotifications();
             notifications.value.forEach(function (notification){
                 console.log('notification.read_at' + notification.read_at);
                if(notification.read_at === null){
