7a06efe866bbb66668933f3e0d27bf5daf4e8217
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-03 10:09:46 +0200

    cc

--- resources/js/pages/Notifications.vue
+++ resources/js/pages/Notifications.vue
@@ -1,70 +1,6 @@
 <template>
     <div>
     <div class="col-span-12 lg:col-span-9 xxl:col-span-10">
-        <!-- BEGIN: Inbox Filter -->
-        <div class="intro-y flex flex-col-reverse sm:flex-row items-center">
-            <div class="w-full sm:w-auto relative mr-auto mt-3 sm:mt-0">
-                <i class="w-4 h-4 absolute my-auto inset-y-0 ml-3 left-0 z-10 text-gray-700 dark:text-gray-300" data-feather="search"></i>
-                <input type="text" class="form-control w-full sm:w-64 box px-10 text-gray-700 dark:text-gray-300 placeholder-theme-13" placeholder="Search mail">
-                <div class="inbox-filter dropdown absolute inset-y-0 mr-3 right-0 flex items-center" data-placement="bottom-start">
-                    <i class="dropdown-toggle w-4 h-4 cursor-pointer text-gray-700 dark:text-gray-300" role="button" aria-expanded="false" data-feather="chevron-down"></i>
-                    <div class="inbox-filter__dropdown-menu dropdown-menu pt-2">
-                        <div class="dropdown-menu__content box p-5">
-                            <div class="grid grid-cols-12 gap-4 gap-y-3">
-                                <div class="col-span-6">
-                                    <label for="input-filter-1" class="form-label text-xs">From</label>
-                                    <input id="input-filter-1" type="text" class="form-control flex-1" placeholder="example@gmail.com">
-                                </div>
-                                <div class="col-span-6">
-                                    <label for="input-filter-2" class="form-label text-xs">To</label>
-                                    <input id="input-filter-2" type="text" class="form-control flex-1" placeholder="example@gmail.com">
-                                </div>
-                                <div class="col-span-6">
-                                    <label for="input-filter-3" class="form-label text-xs">Subject</label>
-                                    <input id="input-filter-3" type="text" class="form-control flex-1" placeholder="Important Meeting">
-                                </div>
-                                <div class="col-span-6">
-                                    <label for="input-filter-4" class="form-label text-xs">Has the Words</label>
-                                    <input id="input-filter-4" type="text" class="form-control flex-1" placeholder="Job, Work, Documentation">
-                                </div>
-                                <div class="col-span-6">
-                                    <label for="input-filter-5" class="form-label text-xs">Doesn't Have</label>
-                                    <input id="input-filter-5" type="text" class="form-control flex-1" placeholder="Job, Work, Documentation">
-                                </div>
-                                <div class="col-span-6">
-                                    <label for="input-filter-6" class="form-label text-xs">Size</label>
-                                    <select id="input-filter-6" class="form-select flex-1">
-                                        <option>10</option>
-                                        <option>25</option>
-                                        <option>35</option>
-                                        <option>50</option>
-                                    </select>
-                                </div>
-                                <div class="col-span-12 flex items-center mt-3">
-                                    <button class="btn btn-secondary w-32 ml-auto">Create Filter</button>
-                                    <button class="btn btn-primary w-32 ml-2">Search</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full sm:w-auto flex">
-                <button class="btn btn-primary shadow-md mr-2">Start a Video Call</button>
-                <div class="dropdown">
-                    <button class="dropdown-toggle btn px-2 box text-gray-700 dark:text-gray-300" aria-expanded="false">
-                        <span class="w-5 h-5 flex items-center justify-center"> <i class="w-4 h-4" data-feather="plus"></i> </span>
-                    </button>
-                    <div class="dropdown-menu w-40">
-                        <div class="dropdown-menu__content box dark:bg-dark-1 p-2">
-                            <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="user" class="w-4 h-4 mr-2"></i> Contacts </a>
-                            <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"> <i data-feather="settings" class="w-4 h-4 mr-2"></i> Settings </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- END: Inbox Filter -->
         <!-- BEGIN: Inbox Content -->
         <div class="intro-y inbox box mt-5">
             <div class="p-5 flex flex-col-reverse sm:flex-row text-gray-600 border-b border-gray-200 dark:border-dark-1">
@@ -441,7 +377,6 @@
 
 <script>
 import {defineComponent, onMounted, ref, computed, getCurrentInstance, watch} from "vue";
-import store, {useStore} from "../../store";
 import router from '../router';
 import GetNotifications from "../compositions/GetNotifications"
 import GetInvites from "../compositions/GetInvites"
