10529368ccabfcaeecaca9e97103d9c112dd7907
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-04 10:32:48 +0200

    cc

--- resources/js/pages/Notifications.vue
+++ resources/js/pages/Notifications.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-    <div class="col-span-12 lg:col-span-9 xxl:col-span-10">
+        <div class="col-span-12 lg:col-span-9 xxl:col-span-10">
         <!-- BEGIN: Inbox Content -->
         <div class="intro-y inbox box mt-5">
             <div class="overflow-x-auto sm:overflow-x-visible">
@@ -30,6 +30,36 @@
         </div>
         <!-- END: Inbox Content -->
     </div>
+        <div class="col-span-12 lg:col-span-9 xxl:col-span-10">
+            <!-- BEGIN: Inbox Content -->
+            <div class="intro-y inbox box mt-5">
+                <div class="overflow-x-auto sm:overflow-x-visible">
+                    <div v-for="(notification, index) in notificationsComp"
+                         :key="'notification_' + index"
+                         class="intro-y pb-2">
+                        <div :class="(notification.read_at === null) ? 'inbox__item inline-block sm:block text-gray-700 dark:text-gray-500 bg-gray-100 dark:bg-dark-1 border-b border-gray-200 dark:border-dark-1' : 'inbox__item inline-block sm:block text-gray-700 dark:text-gray-500 bg-gray-100 dark:bg-dark-1 border-b border-gray-200 dark:border-dark-1 opacity-60'">
+                            <div class="flex px-5 py-3">
+                                <div class="w-72 flex-none flex items-center mr-5">
+                                    <!--                                <input class="form-check-input flex-none" type="checkbox" checked>-->
+                                    <a href="javascript:;" class="w-5 h-5 flex-none ml-4 flex items-center justify-center text-gray-500"> <i class="w-4 h-4" data-feather="star"></i> </a>
+                                    <a href="javascript:;" class="w-5 h-5 flex-none ml-2 flex items-center justify-center text-gray-500"> <i class="w-4 h-4" data-feather="bookmark"></i> </a>
+                                    <div class="w-6 h-6 flex-none image-fit relative ml-5">
+                                        <Avatar :src="'/s3/avatars/' + notification.data.author.avatar"
+                                                :username="notification.data.author.name + ' ' + notification.data.author.lastname"
+                                                :size="35" color="#FFF" background-color="#930f68"/>
+                                    </div>
+                                </div>
+                                <div class="w-64 sm:w-auto truncate" @click="goTo(notification.data.name,notification.id,notification.data.params,notification.data.id)">
+                                    <span class="inbox__item--highlight">{{ notification.data.message }}</span>
+                                </div>
+                                <div class="inbox__item--time whitespace-nowrap ml-auto pl-10">{{ $dayjs(notification.created_at).format('DD.MM.YYYY HH:mm') }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END: Inbox Content -->
+        </div>
     </div>
 </template>
 
