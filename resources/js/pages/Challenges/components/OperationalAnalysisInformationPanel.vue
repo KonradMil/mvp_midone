1a59c1efc1b52c10bc9f570e83f528e7e6d8b4ce
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-05 11:17:30 +0200

    cc

--- resources/js/pages/Challenges/components/OperationalAnalysisInformationPanel.vue
+++ resources/js/pages/Challenges/components/OperationalAnalysisInformationPanel.vue
@@ -117,7 +117,7 @@ export default {
                         console.log('operational_analyses.value before' + operational_analyses.value);
                         operational_analyses.value = response.data.payload;
                         console.log('operational_analyses.value after' + operational_analyses.value);
-                        console.log('operational_analyses.value after' + operational_analyses.value.time_available_before);
+                        console.log('operational_analyses.value after' + operational_analyses.value[0].time_available_before);
                     } else {
 
                     }
