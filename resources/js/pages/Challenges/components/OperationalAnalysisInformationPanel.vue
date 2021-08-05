173d7afeeffd647a4869b0b501233cd24cdcbd82
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-05 10:41:40 +0200

    cc

--- resources/js/pages/Challenges/components/OperationalAnalysisInformationPanel.vue
+++ resources/js/pages/Challenges/components/OperationalAnalysisInformationPanel.vue
@@ -102,7 +102,7 @@ export default {
     },
 
     setup(props) {
-        const operational_analyses = ref('');
+        const operational_analyses = ref({});
 
         const getOperationalAnalysis = () => {
             axios.post('/api/solution/operational-analyses/get', {id: props.solution.id})
