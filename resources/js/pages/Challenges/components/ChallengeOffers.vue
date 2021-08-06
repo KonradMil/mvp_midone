f5d3144f1382e8f2683bb398c3d497d675a04ee0
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-06 10:59:13 +0200

    cc

--- resources/js/pages/Challenges/components/ChallengeOffers.vue
+++ resources/js/pages/Challenges/components/ChallengeOffers.vue
@@ -365,6 +365,7 @@ export default {
                         offer.rejected = 0;
                         offer.solution.selected_offer_id = offer.id;
                         props.challenge.selected_offer_id = offer.id;
+                        router.push( {path : '/projects/card/' + props.challenge.id});
                     } else {
                         // toast.error(response.data.message);
                     }
