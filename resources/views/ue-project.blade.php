80aed11bf969688af42c7a5c90b08b86e40f63c9
konradmilewski <konrad@kmilewski.eu>
2021-08-10 13:47:50 +0200

    [CRITICAL STUPIDITY] add missing webpage for UE

--- resources/views/ue-project.blade.php
+++ resources/views/ue-project.blade.php
@@ -11421,7 +11421,7 @@
     </style>
 </head>
 <body>
-<div id="app">
+<div>
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
