59b3218bde38b6f0a8098d7141585ce886c97557
konradmilewski <konrad@kmilewski.eu>
2021-08-23 12:24:13 +0200

    [ROUTES] fix merge

--- routes/api/knowledgebase/main.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('knowledgebase/posts')->middleware('auth:sanctum')->group(base_path('routes/api/knowledgebase/posts.php'));
-
-Route::group(['prefix' => 'knowledgebase', 'middleware' => 'auth:sanctum'], function () {
-
-});
--- routes/api/social/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('social/posts')->middleware('auth:sanctum')->group(base_path('routes/api/social/posts.php'));
-Route::prefix('social/threads')->middleware('auth:sanctum')->group(base_path('routes/api/social/threads.php'));
-
-Route::group(['prefix' => 'social', 'middleware' => 'auth:sanctum'], function () {
-
-});
