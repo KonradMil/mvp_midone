44dc891c4e605a130157069e7865838d02ad1acf
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-25 13:25:58 +0200

    [PROJECT_V1] process 1.1 (offer)

--- resources/js/pages/Challenges/components/TechnicalInformationPanel.vue
+++ resources/js/pages/Challenges/components/TechnicalInformationPanel.vue
@@ -45,7 +45,7 @@
                                     id="input-wizard-1"
                                     v-model="details['select_pick_quality'][challenge.technical_details.pick_quality].name"
                                     :options="{locale: 'pl', placeholder: 'Wybierz...', limit: 'Nie można wybrać więcej', search: false, hideSelected: false, classNames: 'w-full' }">
-                                    <option  :selected="details.select_detail_weight === '' ? '' : 'selected'" disabled>Wybierz...</option>
+                                    <option  :selected="details.select_pick_quality === '' ? '' : 'selected'" disabled>Wybierz...</option>
                                     <option :selected="det.value === details.select_pick_quality ? '' : 'selected'" v-for="(det,index) in details.select_pick_quality"
                                             :value="det.value">{{ det.name }}
                                     </option>
