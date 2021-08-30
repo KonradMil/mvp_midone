c28cb924fd44b33732ebf2af80327afe70bb0149
konradmilewski <konrad@kmilewski.eu>
2021-08-30 10:44:02 +0200

    [DEBUG] unity

--- resources/js/pages/Unity/composables/ActionsOutgoing.js
+++ resources/js/pages/Unity/composables/ActionsOutgoing.js
@@ -179,9 +179,11 @@ export default function unityActionOutgoing(gameWindow) {
     function loadStructure (val) {
         console.log('HEEREE IMPORTANT', val);
         if(val && Object.keys(val).length === 0 && val.constructor === Object) {
-            val = JSON.stringify({});
+
+        } else {
+            outgoing(game, 'LoadStructure', val, true);
         }
-        outgoing(game, 'LoadStructure', val, true);
+
     }
 
     function getParts () {
