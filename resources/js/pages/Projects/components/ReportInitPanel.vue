b4d3f361dbd89ba5e802e215c1efa09a678aa5c8
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-30 13:06:04 +0200

    [PROJECT_V1] ReportInitPanel

--- resources/js/pages/Projects/components/ReportInitPanel.vue
+++ resources/js/pages/Projects/components/ReportInitPanel.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="intro-y box lg:mt-5">
+    <div class="intro-y box lg:mt-5" style="width: 350px; height: 250px; margin-top: -2px;">
         <div class="flex items-center p-5 border-b border-gray-200 dark:border-dark-5">
             <h2 class="font-medium text-base mr-auto">
-                {{$t('profiles.agreements')}}
+                Zaakceptuj zmiany żeby przejść do następnej fazy projektu
             </h2>
         </div>
         <!-- END: Display Information -->
@@ -12,8 +12,7 @@
                     id="rodo"
                     type="checkbox"
                     class="form-check-input border mr-2 ring-0"
-                    v-model="project_accept_local_vision"
-                    disabled/>
+                    v-model="project_accept_local_vision"/>
                 <label class="cursor-pointer select-none" for="rodo">
                     Akceptacja wizji lokalnej</label>
             </div>
@@ -32,50 +31,11 @@
                     id="rodo2"
                     type="checkbox"
                     class="form-check-input border mr-2 ring-0"
-                    :checked="project_accept_details"
-                    disabled/>
+                    :checked="project_accept_details"/>
                 <label class="cursor-pointer select-none" for="rodo2">
                     Akceptacja założen projektu
                 </label>
             </div>
-<!--            <div class="border-b border-gray-200 dark:border-dark-5"></div>-->
-<!--            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm"-->
-<!--                 v-if="user.type == 'integrator'">-->
-<!--                <input-->
-<!--                    id="q1"-->
-<!--                    type="checkbox"-->
-<!--                    class="form-check-input border mr-2 ring-0"-->
-<!--                    @click="new_answer = !new_answer"-->
-<!--                    :checked="new_answer"-->
-<!--                />-->
-<!--                <label class="cursor-pointer select-none" for="q1">-->
-<!--                    {{$t('profiles.notifyQuestion')}}-->
-<!--                </label>-->
-<!--            </div>-->
-<!--            <div class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm"-->
-<!--                 v-if="user.type == 'integrator'">-->
-<!--                <input-->
-<!--                    id="q2"-->
-<!--                    type="checkbox"-->
-<!--                    class="form-check-input border mr-2 ring-0"-->
-<!--                    @click="solution_accepted = !solution_accepted"-->
-<!--                    :checked="solution_accepted"/>-->
-<!--                <label class="cursor-pointer select-none" for="q2">-->
-<!--                    {{$t('profiles.informSolution')}}-->
-<!--                </label>-->
-<!--            </div>-->
-<!--            <div v-if="user.type == 'integrator'"-->
-<!--                 class="intro-x flex items-center text-gray-700 dark:text-gray-600 mt-4 text-xs sm:text-sm">-->
-<!--                <input-->
-<!--                    id="q3"-->
-<!--                    type="checkbox"-->
-<!--                    class="form-check-input border mr-2 ring-0"-->
-<!--                    @click="offer_accepted = !offer_accepted"-->
-<!--                    :checked="offer_accepted"/>-->
-<!--                <label class="cursor-pointer select-none" for="q3">-->
-<!--                    {{$t('profiles.informService')}}-->
-<!--                </label>-->
-<!--            </div>-->
             <button class="btn btn-primary w-20 mt-3">{{$t('profiles.save')}}</button>
         </div>
     </div>
