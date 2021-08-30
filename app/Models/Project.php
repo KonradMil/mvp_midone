707a71621f27c32825933ca30d430a7628f7ce58
Lukaszewski <lmioskowski1999@gmail.com>
2021-08-30 15:30:23 +0200

    [PROJECT_V1] ProjectModel

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Casts\JsonAsArrayObjectWithNull;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Spatie\Tags\HasTags;
@@ -16,30 +15,6 @@ class Project extends Model
     use HasTags;
 
     /**
-     *
-     */
-    const TYPES = [
-        "Pick & Place",
-        "Montaż",
-        "Kontrola jakości",
-        "Malowanie",
-        "Spawanie"
-    ];
-
-    /**
-     *
-     */
-    const STAGES = [
-        'draft',
-        'awaiting solution',
-        'awaiting offer',
-        'agreement planning',
-        'project planning',
-        'project accepting',
-        'invoicing'
-    ];
-
-    /**
      * @var string
      */
     public $table = 'projects';
@@ -51,41 +26,6 @@ class Project extends Model
         'type', 'name', 'challenge_id', 'en_name', 'solution_deadline', 'offer_deadline', 'status',
         'stage', 'save_json', 'screenshot_path', 'description', 'en_description',
     ];
-
-    /**
-     * @var string[]
-     */
-    protected $casts = [
-        'name' => 'string',
-        'type' => 'string',
-        'challenge_id' => 'integer',
-        'en_name' => 'string',
-        'description' => 'string',
-        'en_description' => 'string',
-        'status' => 'integer',
-        'stage' => 'integer',
-        'save_json' => JsonAsArrayObjectWithNull::class,
-        'screenshot_path' => 'string',
-        'allowed_publishing' => 'boolean',
-        'solution_deadline' => 'timestamp',
-        'offer_deadline' => 'timestamp',
-    ];
-
-    /**
-     * @return string
-     */
-    public function getStageName(): string
-    {
-        return self::STAGES[$this->attributes['stage']];
-    }
-
-    /**
-     * @return string
-     */
-    public function getTypeName(): string
-    {
-        return self::TYPES[$this->attributes['type']];
-    }
 
     /**
      * @return HasOne
