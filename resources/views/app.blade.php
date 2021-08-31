828f198733c4053f7d514c6ece830f6b90beda16
konradmilewski <konrad@kmilewski.eu>
2021-08-31 11:50:54 +0200

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,10 @@
         window.unity_path = '{{env('UNITY_PATH')}}';
         window.unity_workshop_path = '{{env('UNITY_WORKSHOP_PATH')}}';
         window.unity_tutorial_path = '{{env('UNITY_PATH')}}';
+        window.errors = '{{json_encode(Session::get('error'))}}';
+        window.warnings = '{{json_encode(Session::get('warning'))}}';
+        window.info = '{{json_encode(Session::get('info'))}}';
+        window.success = '{{json_encode(Session::get('success'))}}';
         @php
         if(empty(Auth::user()->companies->toArray())) {
                $company = new App\Models\Company();
