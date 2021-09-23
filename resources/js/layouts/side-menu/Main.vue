ec7f468b47f228fb33d9b4e4f29ab0675a561197
konradmilewski <konrad@kmilewski.eu>
2021-09-23 13:21:24 +0200

    [LOGO] change logos

--- resources/js/layouts/side-menu/Main.vue
+++ resources/js/layouts/side-menu/Main.vue
@@ -7,7 +7,7 @@
                 <!--         BEGIN: Logo-->
                 <div class="flex-row w-full items-center">
                     <router-link :to="{ name: 'dashboard' }" tag="a" class="intro-x flex items-center  pt-4 px-12">
-                        <img alt="DBR77 Platforma Robotów " class="w-full" src="/s3/twopointo/images/dbr_logo_white.svg"/>
+                        <img alt="DBR77 Platforma Robotów " class="w-full" src="/s3/twopointo/images/dbr_logo_white_notagline.svg"/>
                     </router-link>
                 </div>
                 <!--         END: Logo-->
