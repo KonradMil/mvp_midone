8673c29dd234205e64dba6a28b373f266130368a
konradmilewski <konrad@kmilewski.eu>
2021-10-06 00:26:00 +0200

    change url to env

--- app/Notifications/OfferAcceptedNotification.php
+++ app/Notifications/OfferAcceptedNotification.php
@@ -45,7 +45,7 @@ class OfferAcceptedNotification extends Notification
     {
         return (new MailMessage)
             ->line('Oferta została zaakceptowana do wyzwania: ' . $this->challenge->name .'.')
-            ->action('Sprawdź', url('https://staging.appworks-dev.pl/challenges/card/' . $this->challenge->id))
+            ->action('Sprawdź', url(env('APP_PATH') . '/challenges/card/' . $this->challenge->id))
             ->line('Dziękujemy za korzystanie z platformy DBR77!');
     }
 
