7d35e1cca8c33ea828325689069dbb3822725b6d
konradmilewski <konrad@kmilewski.eu>
2021-10-07 15:07:28 +0200

    challange tabs - DEV ver

--- resources/js/pages/Unity/Main.vue
+++ resources/js/pages/Unity/Main.vue
@@ -108,8 +108,6 @@ import BottomPanel from "./components/BottomPanel";
 import RightPanel from "./components/RightPanel";
 import RightButtons from "./components/RightButtons";
 import WorkshopModal from "./WorkshopModal"
-
-import router from "../../router";
 import WebRTC from "./WebRTC";
 
 const ww = WindowWatcher();
