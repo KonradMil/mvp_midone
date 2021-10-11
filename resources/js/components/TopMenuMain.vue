70fca49c8240aab1c9ae097e0c5921a44dca244e
konradmilewski <konrad@kmilewski.eu>
2021-10-11 15:49:02 +0200

    tabs fix

--- resources/js/components/TopMenuMain.vue
+++ resources/js/components/TopMenuMain.vue
@@ -5,7 +5,7 @@
             <li v-for="(menu, menuKey) in formattedMenu" :key="menuKey">
                 <a
                     href="#"
-                    @click.prevent="menuChanged(menu.value)"
+                    @click.prevent="menuChanged(menu)"
                     class="top-menu"
                     :class="{
               'top-menu--active': menu.active
@@ -23,6 +23,26 @@
             <!-- END: First Child -->
         </ul>
     </nav>
+    <div class="intro-y flex flex-col sm:flex-row items-center mt-8">
+        <h2 class="text-lg font-medium mr-auto">{{ $t('challengesMain.challenges') }} <span v-if="active != ''"> {{active}}</span></h2>
+        <div class="w-full sm:w-auto flex mt-4 sm:mt-0">
+            <button class="btn btn-primary shadow-md mr-2" v-if="user.type == 'investor' && type==='normal'" @click="$router.push({name: 'addChallenge'})">{{ $t('challengesMain.addChallenge') }} </button>
+            <div class="dropdown ml-auto sm:ml-0">
+                <div class="dropdown-menu w-40">
+                    <div class="dropdown-menu__content box dark:bg-dark-1 p-2">
+                        <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
+                            <Share2Icon class="w-4 h-4 mr-2"/>
+                            {{ $t('global.sharePost') }}
+                        </a>
+                        <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
+                            <DownloadIcon class="w-4 h-4 mr-2"/>
+                            {{ $t('global.downloadPost') }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -33,8 +53,13 @@ export default {
     emits:[
       'tabChanged'
     ],
+    props: {
+        user: {
+            type: Object
+        }
+    },
     setup(props, {emit}) {
-
+        const active = ref('');
         const formattedMenu = ref([
             {
                 icon: 'ActivityIcon',
@@ -57,13 +82,14 @@ export default {
         ]);
 
         const menuChanged = (val) => {
-
-            emit('tabChanged', val);
+            active.value = val.title;
+            emit('tabChanged', val.value);
         }
 
         return {
             menuChanged,
-            formattedMenu
+            formattedMenu,
+            active
         }
     }
 }
