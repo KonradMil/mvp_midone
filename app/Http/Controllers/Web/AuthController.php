c9d3bf025c2f7ab51bd23e9d213e7b34c98e110c
Krystian Wieczorek <krystian.wieczorek@robug.pl>
2021-10-14 13:25:11 +0200

    Debug fb login

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -80,6 +80,10 @@ class AuthController extends Controller
                     return redirect('/login');
                 }
 
+                if($provider === 'facebook') {
+                    dd($socialUser);
+                }
+
                 /** @var User|null $user */
                 $user = $userRepository->findByEmail($socialUser->getEmail());
 
