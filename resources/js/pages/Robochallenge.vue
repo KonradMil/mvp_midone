7ec58a6c04d4910123f3ff06cffb3958b1251a05
Krystian Wieczorek <krystian.wieczorek@robug.pl>
2021-10-15 16:09:33 +0200

    Add login button to robohakaton page

--- resources/js/pages/Robochallenge.vue
+++ resources/js/pages/Robochallenge.vue
@@ -130,6 +130,9 @@
                             <button class="btn btn-primary py-3 px-4 w-full xl:w-64 xl:mr-3 align-top" @click="handleSubmit">
                                 Wyślij zgłoszenie
                             </button>
+                            <button @click.prevent="$router.push('login')" class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">
+                                {{ $t('login.login') }}
+                            </button>
                         </div>
 
                     </div>
