9283da61fe0f38bbf202f5ed3c54e742155d6236
konradmilewski <konrad@kmilewski.eu>
2021-10-21 13:13:17 +0200

    admin free saves

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -22,12 +22,10 @@ class AdminController extends Controller
                 $query->where($filter['field'], $filter['type'], $filter['value']);
             }
         }
+
         $query->take($size)->offset(($page - 1) * $size);
         $query->with(['author', 'solutionSelected', 'solutionSelected.author']);
-//        dd($query->get());
-//        ,'author.name', 'author.lastname', 'author.email', 'solutionSelected.name', 'solutionSelected.screenshot_path', 'solutionSelected.author.name', 'solutionSelected.author.last_name', 'solutionSelected.author.email'
-//        select(['challenges.name', 'challenges.offer_deadline', 'challenges.solution_deadline', 'challenges.screenshot_path', 'challenges.stage', 'challenges.status', 'challenges.type', 'challenges.created_at'])
-        return response()->json(['last_page' => ceil($query->count() / $size), 'data' => $query->get()]);
+         return response()->json(['last_page' => ceil($query->count() / $size), 'data' => $query->get()]);
     }
 
     public function adminGetFreeSaves(Request $request)
@@ -37,6 +35,7 @@ class AdminController extends Controller
         $filter = $request->get('filters');
 
         $query = FreeSave::query();
+        $query->where('published', '=', 1);
         $query->take($size)->offset(($page - 1) * $size);
         $query->with('getOwner');
         $fss = $query->get();
