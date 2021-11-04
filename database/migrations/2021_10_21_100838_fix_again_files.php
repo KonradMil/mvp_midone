7934ba21851721bd42461a02569443a5a359e16d
konradmilewski <konrad@kmilewski.eu>
2021-11-04 15:28:45 +0100

    straight to production

--- database/migrations/2021_10_21_100838_fix_again_files.php
+++ database/migrations/2021_10_21_100838_fix_again_files.php
@@ -14,6 +14,7 @@ class FixAgainFiles extends Migration
     public function up()
     {
         Schema::table('files', function (Blueprint $table) {
+            $table->unsignedBigInteger('author_id');
             $table->foreign('author_id')->references('id')->on('users');
         });
     }
