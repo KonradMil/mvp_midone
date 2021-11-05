42a7e51b050a783e4f347bae7a742ce233fb92ca
Lukaszewski <lmioskowski1999@gmail.com>
2021-11-05 10:06:18 +0100

    solutionsAll page fix

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -230,7 +230,7 @@ export const routes = [
                 name: 'internalChallenegeCard',
                 path: '/challenges/card/:id',
                 component: Card,
-                props: true
+                props: true,
             },
             {
                 name: 'addSolution',
