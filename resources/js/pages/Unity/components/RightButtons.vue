2a775abe53e72a0525a7352a70b12573efe10eda
konradmilewski <konrad@kmilewski.eu>
2021-11-05 13:45:41 +0100

    this time fix investor not seeing estimates

--- resources/js/pages/Unity/components/RightButtons.vue
+++ resources/js/pages/Unity/components/RightButtons.vue
@@ -13,13 +13,13 @@
             <div class="unity-button">
                 <UnityButton :tooltip="'Założenia finansowe'" :alttext="'financial'" :path="'/s3/builder_icons/assum_simple.png'" :action="'financial'" position="rightbuttonclick"  />
             </div>
-            <div class="unity-button" v-if="type == 'solution' && user.type != 'investor'">
+            <div class="unity-button" v-if="type == 'solution'">
                 <UnityButton :tooltip="'Kosztorys'" :alttext="'financial'" :path="'/s3/builder_icons/estimate_simple.png'" :action="'estimates'" position="rightbuttonclick"  />
             </div>
-            <div class="unity-button" v-if="type == 'solution' && user.type != 'investor'">
+            <div class="unity-button" v-if="type == 'solution'">
                 <UnityButton :tooltip="'Analiza finansowa'" :alttext="'operational'" :path="'/s3/builder_icons/analiza_finansowa.png'" :action="'financialanalysis'" position="rightbuttonclick"  />
             </div>
-            <div class="unity-button" v-if="type == 'solution' && user.type != 'investor'">
+            <div class="unity-button" v-if="type == 'solution'">
                 <UnityButton :tooltip="'Analiza operacyjna'" :alttext="'operationalanalysis'" :path="'/s3/builder_icons/analiza_operacyjna_simple.png'" :action="'operationalanalysis'" position="rightbuttonclick"  />
             </div>
         </div>
